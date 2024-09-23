2b5bff02583f1e45a26d551fb4e9b27309d797ae
Krasilnikov Roman <x0kill@ya.ru>
2024-09-23 14:59:32 +0300

    Null field

--- apps/playground/src/components/form/fields/fields.ts
+++ apps/playground/src/components/form/fields/fields.ts
@@ -1,11 +1,13 @@
 import type { Field, Fields, FieldType } from "./model";
 
 import Root from "./root.svelte";
+import Null from './null.svelte'
 import String from './string.svelte'
 import Number from './number.svelte'
 
 export const registry: { [T in FieldType]: Field<T> } = {
   root: Root,
+  null: Null,
   string: String,
   // TODO: Integer field
   integer: Number,
---
+++ apps/playground/src/components/form/fields/null.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import type { FieldProps } from "./model";
+
+  let { value = $bindable() }: FieldProps<"null"> = $props();
+
+  $effect(() => {
+    if (value === undefined) {
+      value = null;
+    }
+  });
+</script>
