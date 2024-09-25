21bf8a78187b0fa9c5e61b61cc6aa819bf2f9c24
Krasilnikov Roman <x0kill@ya.ru>
2024-09-25 14:58:57 +0300

    Transformation rune docs

--- apps/playground/src/lib/svelte.svelte.ts
+++ apps/playground/src/lib/svelte.svelte.ts
@@ -4,32 +4,38 @@ export type TransformationConfig<V> = {
    */
   transform: (isDependencyRegistrationOnlyCall: boolean) => V;
   guard?: (v: V) => boolean;
-  update?: (v: V) => void;
+  /**
+   * @returns `true` to indicate that input update should not be ignored
+   */
+  update?: (v: V) => (void | boolean);
 };
 
 export function createTransformation<V>(config: TransformationConfig<V>) {
-  let updatedBySuccessor = false;
-  let updatedValue = $state.raw<V>();
-  const transformed = $derived.by(() => {
-    const updated = updatedValue;
-    if (updatedBySuccessor) {
-      updatedBySuccessor = false;
+  let ignoreInputUpdate = false;
+  let lastOutputValue = $state.raw<V>();
+  const output = $derived.by(() => {
+    const trackedLastOutputValue = lastOutputValue;
+    if (ignoreInputUpdate) {
+      ignoreInputUpdate = false;
       config.transform(true);
-      return updated as V;
+      return trackedLastOutputValue as V;
     }
     return config.transform(false);
   });
   return {
     get value() {
-      return transformed;
+      return output;
     },
     set value(value: V) {
       if (config.guard && !config.guard(value)) {
         return;
       }
-      updatedBySuccessor = !Object.is(value, updatedValue);
-      updatedValue = value;
-      config.update?.(value);
+      // NOTE: We should not to ignore input update in two cases:
+      // 1. During input update new value is produced (`true` value returned from `config.update`)
+      // 2. Setter is executed with same value as last one (because `derivedOutput` will not be
+      //    triggered and `ignoreInputUpdate` flag will be stale)
+      ignoreInputUpdate = !config.update?.(value) && !Object.is(value, lastOutputValue);
+      lastOutputValue = value;
     },
   };
 }
