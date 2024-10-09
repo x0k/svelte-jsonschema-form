696f76ab3b8ec935739a14db707fb7b8fd481835
Krasilnikov Roman <x0kill@ya.ru>
2024-10-09 16:42:58 +0300

    Playground link

--- apps/docs/astro.config.mjs
+++ apps/docs/astro.config.mjs
@@ -1,37 +1,52 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
-import svelte from '@astrojs/svelte'
+import { fileURLToPath } from 'node:url'
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://x0k.github.io',
-	base: "/svelte-jsonschema-form/",
-	trailingSlash: 'always',
-	integrations: [
-		svelte(),
-		starlight({
-			title: 'svelte-jsonschema-form',
-			social: {
-				github: 'https://github.com/x0k/svelte-jsonschema-form',
-			},
-			sidebar: [
-				{
-					label: "Concepts",
-					autogenerate: { directory: "concepts" },
-				}
-				// {
-				// 	label: 'Guides',
-				// 	items: [
-				// 		// Each item here is one entry in the navigation menu.
-				// 		{ label: 'Example Guide', slug: 'guides/example' },
-				// 	],
-				// },
-				// {
-				// 	label: 'Reference',
-				// 	autogenerate: { directory: 'reference' },
-				// },
-			],
-		}),
-	],
+  site: "https://x0k.github.io",
+  base: "/svelte-jsonschema-form/",
+  trailingSlash: "always",
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en", "ru"],
+  },
+  integrations: [
+    svelte(),
+    starlight({
+      title: "svelte-jsonschema-form",
+      social: {
+        github: "https://github.com/x0k/svelte-jsonschema-form",
+      },
+      sidebar: [
+        {
+          label: "Concepts",
+          autogenerate: { directory: "concepts" },
+        },
+        // {
+        // 	label: 'Guides',
+        // 	items: [
+        // 		// Each item here is one entry in the navigation menu.
+        // 		{ label: 'Example Guide', slug: 'guides/example' },
+        // 	],
+        // },
+        // {
+        // 	label: 'Reference',
+        // 	autogenerate: { directory: 'reference' },
+        // },
+      ],
+      components: {
+        Header: "./src/components/header-with-links.astro",
+      },
+    }),
+  ],
+  vite: {
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+      },
+    },
+  },
 });
