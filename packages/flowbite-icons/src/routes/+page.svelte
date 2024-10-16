53e23d2b4d2a362a7171810441e9253fd17e5d34
Krasilnikov Roman <x0kill@ya.ru>
2024-10-17 00:05:18 +0300

    Bump deps

--- packages/flowbite-icons/src/routes/+page.svelte
+++ packages/flowbite-icons/src/routes/+page.svelte
@@ -1,16 +1,20 @@
 <script lang="ts">
-	import '../app.css';
-
+	import type { Snippet } from 'svelte';
+	
 	import { icons } from '../lib';
+	import "../app.css"
 </script>
 
-<div style="height: 100vh; width: 100%; display: flex; align-items: center; justify-content: center">
+<div
+	style="height: 100vh; width: 100%; display: flex; align-items: center; justify-content: center"
+>
 	<div style="display: flex; flex-direction: column; gap: 1rem">
 		{#each Object.entries(icons) as [key, icon]}
-			<button style="display: flex; gap: 0.5rem; align-items: center; height: 2rem" >
-				{@render icon()}
+			<button style="display: flex; gap: 0.5rem; align-items: center; height: 2rem">
+				{@render (icon as Snippet)()}
 				{key}
 			</button>
 		{/each}
 	</div>
 </div>
+
