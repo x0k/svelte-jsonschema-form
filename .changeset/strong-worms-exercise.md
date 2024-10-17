337d3d74f479b5164df66f4a8344958e1befc146
Krasilnikov Roman <x0kill@ya.ru>
2024-10-17 12:42:48 +0300

    [docs] Add `Programmatic control` section

---
+++ .changeset/strong-worms-exercise.md
@@ -0,0 +1,5 @@
+---
+"docs": patch
+---
+
+Add `Programmatic control` section
---
+++ apps/docs/src/content/docs/guides/_bind-form.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import type { Schema } from "@sjsf/form";
+
+  import CustomForm from "@/components/custom-form.svelte";
+
+  const schema: Schema = {
+    type: "string",
+  };
+
+  let form = $state<HTMLFormElement>();
+</script>
+
+<CustomForm bind:form {schema} novalidate onSubmit={(v) => window.alert(v)}>
+  {null}
+</CustomForm>
+
+<button
+  onclick={() => {
+    form?.requestSubmit();
+  }}>My submit</button
+>
+
+<button onclick={() => {
+  form?.reset();
+}} >
+  My reset
+</button>
