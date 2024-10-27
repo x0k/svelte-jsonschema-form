ed1219a5d2ea39a4be4e4622a73b01b27929239d
Krasilnikov Roman <x0kill@ya.ru>
2024-10-27 19:04:23 +0300

    WIP: Sync dev deps

--- packages/shadcn-theme/src/lib/components/button-component.svelte
+++ packages/shadcn-theme/src/lib/components/button-component.svelte
@@ -1,12 +1,16 @@
 <script lang="ts">
 	import type { ComponentProps } from '@sjsf/form';
+	import { Button, type ButtonProps } from './ui/button';
 
 	const { children, type, attributes, disabled, onclick }: ComponentProps<'button'> = $props();
 
 	const isSubmit = $derived(type === 'submit');
 </script>
 
-<button
+<Button type={isSubmit ? 'submit' : 'button'} {onclick} {...attributes as ButtonProps} {disabled}>
+	{@render children()}
+</Button>
+<!-- <button
 	class="btn preset-filled"
   class:preset-filled-primary-500={isSubmit}
   class:w-full={isSubmit}
@@ -16,4 +20,4 @@
 	{disabled}
 >
 	{@render children()}
-</button>
+</button> -->
