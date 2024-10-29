1925a68e8c6fb9183354219f5053bb0fa33ba9a4
Krasilnikov Roman <x0kill@ya.ru>
2024-10-29 17:27:08 +0300

    [playground] Use `useForm` API

--- apps/playground/src/form.svelte
+++ apps/playground/src/form.svelte
@@ -1,45 +1,38 @@
-<script lang="ts" generics="T, E">
-  import { SvelteMap } from "svelte/reactivity";
-  import { Form, type FormProps } from "@sjsf/form";
+<script lang="ts">
+  import { type SchemaValue, FormContent, SubmitButton } from "@sjsf/form";
   import { components } from "@sjsf/shadcn-theme/default";
   import { setThemeContext } from "@sjsf/shadcn-theme";
 
   import type { themes } from "./themes";
 
-  interface Props<T> extends FormProps<T, E> {
+  interface Props {
+    html5Validation: boolean;
     themeName: keyof typeof themes;
     lightOrDark: "light" | "dark";
+    form: HTMLFormElement | undefined;
+    value: SchemaValue | undefined;
   }
 
   let {
+    form = $bindable(),
+    value = $bindable(),
     themeName,
     lightOrDark,
-    value = $bindable(),
-    form = $bindable(),
-    isSubmitted = $bindable(false),
-    errors = $bindable(new SvelteMap()),
-    ...rest
-  }: Props<T> = $props();
-
-  let self: Form<T, E>;
-
-  export function validate() {
-    return self.validate();
-  }
+    html5Validation,
+  }: Props = $props();
 
   if (themeName === "shadcn") {
     setThemeContext({ components });
   }
 </script>
 
-<Form
+<form
   data-theme={themeName === "skeleton" ? "cerberus" : lightOrDark}
   class={lightOrDark}
   style="background-color: transparent; display: flex; flex-direction: column; gap: 1rem; padding: 0.3rem;"
-  bind:this={self}
-  {...rest}
-  bind:value
-  bind:form
-  bind:isSubmitted
-  bind:errors
-/>
+  bind:this={form}
+  novalidate={!html5Validation || undefined}
+>
+  <FormContent bind:value />
+  <SubmitButton />
+</form>
