59196ba2c479af7d3ff0cb3a8cded268811133e1
Roman Krasilnikov <x0kill@ya.ru>
2025-02-03 21:23:07 +0300

    [daisy] Fix range widget error class

--- packages/daisyui-theme/src/lib/widgets/number-widget.svelte
+++ packages/daisyui-theme/src/lib/widgets/number-widget.svelte
@@ -2,14 +2,15 @@
 	import type { WidgetProps } from '@sjsf/form';
 
 	let { value = $bindable(), attributes, errors }: WidgetProps<'number'> = $props();
+
+	const isRange = $derived(attributes.type === 'range');
 </script>
 
 <input
 	type="number"
 	bind:value
-	class={attributes.type === 'range'
-		? 'range grow'
-		: 'input input-bordered grow'}
-	class:input-error={errors.length}
+	class={isRange ? 'range grow' : 'input input-bordered grow'}
+	class:input-error={errors.length && !isRange}
+	class:range-error={errors.length && isRange}
 	{...attributes}
 />
