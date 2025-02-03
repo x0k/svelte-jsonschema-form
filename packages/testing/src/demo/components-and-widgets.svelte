9c838116302471e28620784c310c20d1d632a2db
Roman Krasilnikov <x0kill@ya.ru>
2025-02-03 16:03:21 +0300

    [testing] Fix widgets errors, use svelte-json-tree for data inspection

--- packages/testing/src/demo/components-and-widgets.svelte
+++ packages/testing/src/demo/components-and-widgets.svelte
@@ -1,40 +1,55 @@
 <script lang="ts">
-  import { createForm, type Theme } from '@sjsf/form';
-  import { translation } from '@sjsf/form/translations/en';
+  import {
+    createForm3,
+    DEFAULT_ID_PREFIX,
+    DEFAULT_ID_SEPARATOR,
+    pathToId,
+    type Theme,
+  } from "@sjsf/form";
+  import { translation } from "@sjsf/form/translations/en";
 
-  import * as widgets from './widgets';
-  import * as components from './components';
-  import { validator } from './ajv-validator';
-  import Form from './form.svelte';
+  import * as widgets from "./widgets";
+  import * as components from "./components";
+  import { validator } from "./ajv-validator";
+  import Form from "./form.svelte";
 
   const { theme }: { theme: Theme } = $props();
 
-  const [widgetsCtx, widgetsForm] = createForm({
+  const widgetsForm = createForm3({
     ...theme,
     schema: widgets.schema,
     uiSchema: widgets.uiSchema,
+    initialErrors: widgets.errors,
     validator,
     translation,
-  })
+  });
 
-  const [componentsCtx, componentsForm] = createForm({
+  const componentsForm = createForm3({
     ...theme,
     initialValue: {
-      array: ['fixed', 123],
-      additional: 'value'
+      array: ["fixed", 123],
+      additional: "value",
     },
+    initialErrors: [
+      {
+        instanceId: pathToId(DEFAULT_ID_PREFIX, DEFAULT_ID_SEPARATOR, []),
+        message: "message",
+        propertyTitle: "Title",
+        error: null,
+      },
+    ],
     schema: components.schema,
     uiSchema: components.uiSchema,
     validator,
     translation,
-  })
+  });
 </script>
 
 <div style="display: flex; gap: 2rem; padding: 2rem;">
   <div style="display: flex; flex-direction: column; flex: 1; gap: 1rem">
-    <Form context={widgetsCtx} form={widgetsForm} />
+    <Form form={widgetsForm} />
   </div>
   <div style="display: flex; flex-direction: column; flex: 1; gap: 1rem">
-    <Form context={componentsCtx} form={componentsForm} />
+    <Form form={componentsForm} />
   </div>
 </div>
