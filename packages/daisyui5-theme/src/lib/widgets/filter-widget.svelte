c2949a47c3a9af51f2ee099a5b32b590b9c4cbba
Roman Krasilnikov <x0kill@ya.ru>
2025-02-04 01:28:11 +0300

    [daisy5] WIP: Add filter widget

---
+++ packages/daisyui5-theme/src/lib/widgets/filter-widget.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	import { type WidgetProps, indexMapper, singleOption } from '@sjsf/form';
+
+	let { attributes, value = $bindable(), options, errors }: WidgetProps<'radio'> = $props();
+
+	const mapped = singleOption({
+		mapper: () => indexMapper(options),
+		value: () => value,
+		update: (v) => (value = v)
+	});
+</script>
+
+<div class="filter">
+	<input
+		class="btn filter-reset"
+		type="radio"
+		bind:group={mapped.value}
+		{...attributes}
+		aria-label="Reset"
+	/>
+	{#each options as option, index (option.id)}
+		<input
+			type="radio"
+			class="btn"
+			bind:group={mapped.value}
+			value={index}
+			aria-label={option.label}
+			{...attributes}
+			id={option.id}
+			disabled={option.disabled || attributes.disabled}
+		/>
+	{/each}
+</div>
