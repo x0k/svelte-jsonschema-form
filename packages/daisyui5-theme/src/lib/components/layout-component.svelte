c91b6818761fa032d7b9afe8b4ca783745280a22
Roman Krasilnikov <x0kill@ya.ru>
2025-02-03 22:07:10 +0300

    [daisy5] Simplify layout component

--- packages/daisyui5-theme/src/lib/components/layout-component.svelte
+++ packages/daisyui5-theme/src/lib/components/layout-component.svelte
@@ -1,44 +1,38 @@
 <script lang="ts">
+	import type { HTMLAttributes, HTMLFieldsetAttributes } from 'svelte/elements';
 	import type { ComponentProps } from '@sjsf/form';
 
 	const { type, children, attributes }: ComponentProps<'layout'> = $props();
 
-	const isItem = $derived(
-		type === 'array-item' || type === 'object-property' || type === 'field-content'
-	);
+	const isItem = $derived(type === 'array-item' || type === 'object-property');
 	const isControls = $derived(type === 'array-item-controls');
-	const isGrowable = $derived(
-		type === 'array-item-content' ||
-			type === 'object-property-key-input' ||
-			type === 'object-property-content'
-	);
-	const isField = $derived(type === 'multi-field' || type === 'field');
 	const isColumn = $derived(
-		type === 'array-field' ||
-			type === 'object-field' ||
-			type === 'array-items' ||
-			type === 'object-properties' ||
-			type === 'root-field'
+		type === 'array-items' || type === 'object-properties' || type === 'multi-field'
 	);
-	const isMultiFieldControls = $derived(type === 'multi-field-controls');
 </script>
 
-{#if type === 'field-meta' || type === 'field-content' ||
-		 type === 'root-field' || type === 'array-field-meta' || type === 'object-field-meta'}
+{#if type === 'field-meta' || type === 'field-content' || type === 'root-field' || type === 'array-field-meta' || type === 'object-field-meta'}
 	{@render children()}
 {:else if type === 'field' || type === 'array-field' || type === 'object-field'}
-	<fieldset class="fieldset gap-y-2">
+	<fieldset
+		class="fieldset gap-y-2"
+		data-layout={type}
+		{...attributes as HTMLAttributes<HTMLDivElement> & HTMLFieldsetAttributes}
+	>
 		{@render children()}
 	</fieldset>
 {:else}
 	<div
 		class={{
-			'flex': isItem || isControls || isField || isColumn || isMultiFieldControls,
-			'gap-2': isItem || isField || isColumn,
+			flex: isItem || isControls || isColumn || type === 'multi-field-controls',
+			'gap-2': isItem || isColumn,
 			'items-start': isItem || isControls,
-			'join': isControls,
-			'grow': isGrowable,
-			'flex-col': isColumn || isField
+			join: isControls,
+			grow:
+				type === 'array-item-content' ||
+				type === 'object-property-key-input' ||
+				type === 'object-property-content',
+			'flex-col': isColumn
 		}}
 		data-layout={type}
 		{...attributes}
