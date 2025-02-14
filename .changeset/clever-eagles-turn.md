3f780cab808f9316911d9b13b39b7ce159c8f234
Roman Krasilnikov <x0kill@ya.ru>
2025-02-15 00:35:20 +0300

    [lib] Remove `deep-equal` submodule

---
+++ .changeset/clever-eagles-turn.md
@@ -0,0 +1,5 @@
+---
+"@sjsf/form": major
+---
+
+Remove `lib/deep-equal` submodule
--- packages/form/src/lib/deep-equal.ts
+++
@@ -1,6 +0,0 @@
-import { deepEqual } from "fast-equals";
-
-export {
-  /** @deprecated use `isSchemaValueDeepEqual` or `isSchemaDeepEqual` from `form/core` */
-  deepEqual,
-};
