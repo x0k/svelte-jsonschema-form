a9dc8591f6e7cc240b947cded0dca2fd1280b622
Roman Krasilnikov <x0kill@ya.ru>
2025-02-15 15:13:48 +0300

    [form] Remove `asSnippet` function

--- packages/form/src/form/icons.ts
+++ packages/form/src/form/icons.ts
@@ -1,25 +1,7 @@
-import type {
-  Component,
-  ComponentInternals,
-  ComponentProps,
-  Snippet,
-} from "svelte";
+import type { Snippet } from "svelte";
 
 import type { Label, Labels } from "./translation.js";
 
 export type Icons = {
   [L in Label]?: Snippet<[[L, ...Labels[L]]]>;
 };
-
-// @deprecated
-// TODO: remove this function and use snippets export
-// introduced in svelte v5.5.0
-export function asSnippet<C extends Component>(
-  Component: C,
-  props: Partial<ComponentProps<C>> = {}
-): Snippet {
-  // @ts-expect-error
-  return (internals: ComponentInternals) => {
-    Component(internals, props);
-  };
-}
