ba581e6b681e5096a81fca864db83f9c6943032b
Roman Krasilnikov <x0kill@ya.ru>
2025-02-21 18:13:15 +0300

    [playground] WIP: Disable cfworker validator

--- apps/playground2/src/validators.ts
+++ apps/playground2/src/validators.ts
@@ -1,7 +1,7 @@
-import { createValidator2 as ajv8 } from "@sjsf/ajv8-validator";
-import { createValidator as cfworker } from "@sjsf/cfworker-validator";
+import { createSyncFormValidator as ajv8 } from "@sjsf/ajv8-validator";
+// import { createValidator as cfworker } from "@sjsf/cfworker-validator";
 
 export const validators = {
   ajv8,
-  cfworker,
+  // cfworker,
 };
