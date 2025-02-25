7e5667c8dff41c42daafcc15b98c8393f31ceafa
Roman Krasilnikov <x0kill@ya.ru>
2025-02-25 06:00:22 +0300

    [lib] Simplify `dynamic` lib

--- packages/form/src/lib/dynamic.svelte
+++ packages/form/src/lib/dynamic.svelte
@@ -1,51 +1,53 @@
 <script lang="ts" module>
-	import type { Component, ComponentInternals, ComponentProps, Snippet } from 'svelte';
+  import type {
+    Component,
+    ComponentInternals,
+    ComponentProps,
+    Snippet,
+  } from "svelte";
 
-	import Dynamic from './dynamic.svelte';
+  import Dynamic from "./dynamic.svelte";
 
-	export type AnySvelteComponent = Component<any, any, any>
+  export interface DynamicOptions<E> {
+    fallback?: Snippet;
+    onerror?: Snippet<[E]>;
+  }
 
-	export interface DynamicOptions<E> {
-		fallback?: Snippet;
-		onerror?: Snippet<[E]>;
-	}
-
-	export function dynamic<C extends AnySvelteComponent, E>(
-		loader: () => Promise<{ default: C }>,
-		{ fallback, onerror }: DynamicOptions<E> = {}
-	): C {
-		let promise: Promise<Snippet> | undefined;
-		return ((internals, props: ComponentProps<C>) =>
-			Dynamic(internals, {
-				componentPromise: () =>
-					(promise ??= loader().then(
-						(m) =>
-							((internals: ComponentInternals) => {
-								m.default(internals, props);
-							}) as unknown as Snippet
-					)),
-				fallback,
-				onerror
-			})) as C;
-	}
+  export function dynamic<C extends Component<any, any, any>, E>(
+    loader: () => Promise<{ default: C }>,
+    options?: DynamicOptions<E>
+  ): C {
+    let promise: Promise<Snippet> | undefined;
+    return ((internals, props: ComponentProps<C>) =>
+      Dynamic(internals, {
+        ...options,
+        componentPromise: () =>
+          (promise ??= loader().then(
+            (m) =>
+              ((internals: ComponentInternals) => {
+                m.default(internals, props);
+              }) as unknown as Snippet
+          )),
+      })) as C;
+  }
 </script>
 
 <script lang="ts" generics="E">
-	const {
-		componentPromise,
-		fallback,
-		onerror
-	}: {
-		componentPromise: () => Promise<Snippet>;
-		fallback?: Snippet;
-		onerror?: Snippet<[E]>;
-	} = $props();
+  const {
+    componentPromise,
+    fallback,
+    onerror,
+  }: {
+    componentPromise: () => Promise<Snippet>;
+    fallback?: Snippet;
+    onerror?: Snippet<[E]>;
+  } = $props();
 </script>
 
 {#await componentPromise()}
-	{@render fallback?.()}
+  {@render fallback?.()}
 {:then component}
-	{@render component()}
+  {@render component()}
 {:catch e}
-	{@render onerror?.(e)}
+  {@render onerror?.(e)}
 {/await}
