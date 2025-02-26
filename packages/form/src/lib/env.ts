a64ca89983b0be1242d10ff10fc98fb937a18423
Roman Krasilnikov <x0kill@ya.ru>
2025-02-26 13:05:55 +0300

    [lib] Add `clientOnly` component factory

--- packages/form/src/lib/env.ts
+++ packages/form/src/lib/env.ts
@@ -1,2 +1,17 @@
-export const isBrowser =
-  typeof window !== "undefined" && typeof document !== "undefined";
+import type { Component } from 'svelte';
+import { BROWSER } from 'esm-env'
+
+import { dynamic, type DynamicOptions } from './dynamic.svelte'
+
+export class EnvError {}
+
+const REJECTED = Promise.reject(new EnvError())
+
+const rejected = () => REJECTED
+
+export function clientOnly<C extends Component<any, any, any>, E>(
+  loader: () => Promise<{ default: C }>,
+  options?: DynamicOptions<E | EnvError>
+) {
+  return dynamic(BROWSER ? loader : rejected, options)
+}
