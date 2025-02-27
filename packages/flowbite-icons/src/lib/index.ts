cedba8893cfc31940095f68b30dbfa83a7a8cb9c
Roman Krasilnikov <x0kill@ya.ru>
2025-02-27 20:04:48 +0300

    [icons] Fix icons types

--- packages/flowbite-icons/src/lib/index.ts
+++ packages/flowbite-icons/src/lib/index.ts
@@ -3,12 +3,12 @@ import type { Icons } from '@sjsf/form';
 
 import { arrowDownOutline, arrowUpOutline, fileCopyOutline, trashBinOutline } from './icons.svelte';
 
-const icons: Partial<Icons> = {
+const icons = {
 	'move-array-item-up': arrowUpOutline,
 	'move-array-item-down': arrowDownOutline,
 	'remove-array-item': trashBinOutline,
 	'copy-array-item': fileCopyOutline,
 	'remove-object-property': trashBinOutline
-};
+} satisfies Partial<Icons>;
 
-export const iconsResolver = fromRecord(icons)
+export const iconsResolver = fromRecord(icons);
