99f3d8a4a8b0905a9aa149c194c17f8a4760bcbc
Roman Krasilnikov <x0kill@ya.ru>
2025-02-27 14:55:30 +0300

    [basic] Fix definitions

--- packages/basic-theme/src/definitions.ts
+++ packages/basic-theme/src/definitions.ts
@@ -1,5 +1,4 @@
-import { noopComponent } from "@sjsf/form/lib/svelte.svelte";
-import type { Definitions } from "@sjsf/form";
+import { type Definitions, createMessage } from "@sjsf/form";
 import * as fields from "@sjsf/legacy-fields/exports";
 import * as templates from "@sjsf/legacy-templates/exports";
 
@@ -7,10 +6,10 @@ import * as components from "./components/exports.js";
 import * as widgets from "./widgets/exports.js";
 
 export const definitions: Definitions = {
+  multiSelectWidget: createMessage('widget "multiSelectWidget" is missing'),
+  textareaWidget: createMessage('widget "textareaWidget" is missing'),
   ...fields,
   ...templates,
   ...components,
   ...widgets,
-  multiSelectWidget: noopComponent,
-  textareaWidget: noopComponent,
 };
