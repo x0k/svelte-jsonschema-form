20592c03d504c3353a2ab617ddf10c49a9f191ce
Roman Krasilnikov <x0kill@ya.ru>
2025-03-02 02:41:19 +0300

    [daisyui] Setup demo page

--- packages/daisyui-theme/src/routes/+page.svelte
+++ packages/daisyui-theme/src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { ComponentsAndWidgets } from 'testing/demo';
+	import { ComponentsAndWidgets, s } from 'testing/demo';
 
 	import '../app.css';
 	import { theme } from '../lib/index.js';
@@ -9,6 +9,17 @@
 	import '../lib/extra-widgets/date-picker-include';
 	import '../lib/extra-widgets/range-include';
 	import '../lib/extra-widgets/radio-buttons-include';
+	import '../lib/extra-widgets/radio-include';
 </script>
 
-<ComponentsAndWidgets {theme} />
+<ComponentsAndWidgets
+	{theme}
+	additionalSpecs={{
+		multiSelect: [s.uniqueArray, { 'ui:components': { checkboxesWidget: 'multiSelectWidget' } }],
+		radio: [s.enumeration, { 'ui:components': { selectWidget: 'radioWidget' } }],
+		datePicker: [s.text, { 'ui:components': { textWidget: 'datePickerWidget' } }],
+		radioButtons: [s.enumeration, { 'ui:components': { selectWidget: 'radioButtonsWidget' } }],
+		switch: [s.boolean, { 'ui:components': { checkboxWidget: 'switchWidget' } }],
+		textarea: [s.text, { 'ui:components': { textWidget: 'textareaWidget' } }]
+	}}
+/>
