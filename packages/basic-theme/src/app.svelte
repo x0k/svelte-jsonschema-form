eae7bdc0f777bffdcea4ce010754505613bbf0f7
Roman Krasilnikov <x0kill@ya.ru>
2025-03-01 13:32:15 +0300

    [basic] Enable extra widgets for demo

--- packages/basic-theme/src/app.svelte
+++ packages/basic-theme/src/app.svelte
@@ -4,6 +4,8 @@
   import { theme } from "./index.js";
   import "./extra-widgets/multi-select-include.js";
   import "./extra-widgets/textarea-include.js";
+  import "./extra-widgets/date-picker-include.js";
+  import "./extra-widgets/range-include.js";
 </script>
 
 <ComponentsAndWidgets {theme} />
