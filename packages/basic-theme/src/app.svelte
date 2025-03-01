c9d3d1452a44f6561a45624953ca112bebef8356
Roman Krasilnikov <x0kill@ya.ru>
2025-03-01 20:16:57 +0300

    [basic] Fix demo page

--- packages/basic-theme/src/app.svelte
+++ packages/basic-theme/src/app.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import { ComponentsAndWidgets } from "testing/demo";
+  import { ComponentsAndWidgets, s } from "testing/demo";
 
   import { theme } from "./index.js";
   import "./extra-widgets/multi-select-include.js";
@@ -8,4 +8,18 @@
   import "./extra-widgets/range-include.js";
 </script>
 
-<ComponentsAndWidgets {theme} />
+<ComponentsAndWidgets
+  {theme}
+  additionalSpecs={{
+    multiSelect: [
+      s.uniqueArray,
+      { "ui:components": { checkboxesWidget: "multiSelectWidget" } },
+    ],
+    textarea: [s.text, { "ui:components": { textWidget: "textareaWidget" } }],
+    datePicker: [
+      s.text,
+      { "ui:components": { textWidget: "datePickerWidget" } },
+    ],
+    range: [s.number, { "ui:components": { numberWidget: "rangeWidget" } }],
+  }}
+/>
