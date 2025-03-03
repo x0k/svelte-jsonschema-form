ba8b8626586add058a6e483b5cd318e74bf117c9
Roman Krasilnikov <x0kill@ya.ru>
2025-03-03 12:03:45 +0300

    [skeleton] Add date picker widget (extra)

---
+++ packages/skeleton-theme/src/lib/extra-widgets/date-picker-include.ts
@@ -0,0 +1,6 @@
+import { extendable } from '../definitions';
+
+import DatePicker from './date-picker.svelte';
+import './date-picker.svelte';
+
+extendable.datePickerWidget = DatePicker;
---
+++ packages/skeleton-theme/src/lib/extra-widgets/date-picker.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { getFormContext, inputAttributes, type ComponentProps } from '@sjsf/form';
+	import '@sjsf/basic-theme/extra-widgets/date-picker.svelte';
+
+	let { value = $bindable(), config, handlers }: ComponentProps['datePickerWidget'] = $props();
+
+	const ctx = getFormContext();
+
+	const attributes = $derived(inputAttributes(ctx, config, handlers, config.uiOptions?.datePicker));
+</script>
+
+<input type="date" bind:value class="input" {...attributes} />
