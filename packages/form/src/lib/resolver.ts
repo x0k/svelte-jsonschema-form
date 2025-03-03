d670fbec1bf23edbe43ee953599cc3385af2b009
Roman Krasilnikov <x0kill@ya.ru>
2025-03-03 10:32:09 +0300

    [lib] Refactor resolver lib

--- packages/form/src/lib/resolver.ts
+++ packages/form/src/lib/resolver.ts
@@ -1,27 +1,38 @@
 import type { AnyKey } from "./types.js";
 
+export type Resolved<
+  T extends AnyKey,
+  R extends Record<AnyKey, any>,
+> = R[T] extends never ? undefined : R[T];
+
 export type Resolver<
   C extends Record<AnyKey, any>,
-  R extends Record<K, any>,
-  NotFound = never,
-  K extends AnyKey = keyof C,
-> = <T extends K>(type: T, config: C[T]) => R[T] | NotFound;
+  R extends Record<AnyKey, any>,
+> = <T extends keyof C>(type: T, config: C[T]) => Resolved<T, R>;
 
 export function chain<
-  C extends Record<AnyKey, any>,
-  R extends Record<K, any>,
-  NotFound,
-  K extends AnyKey = keyof C,
+  C1 extends Record<AnyKey, any>,
+  R1 extends Record<AnyKey, any>,
+  C2 extends Record<AnyKey, any>,
+  R2 extends Record<AnyKey, any>,
 >(
-  source: Resolver<C, R, undefined, K>,
-  fallback: Resolver<C, R, NotFound, K>
-): Resolver<C, R, NotFound, K> {
-  return <T extends K>(type: T, c: C[T]) =>
+  source: Resolver<C1, R1>,
+  fallback: Resolver<C2, R2>
+): Resolver<C1 & C2, R1 & R2> {
+  return <T extends keyof (C1 & C2)>(type: T, c: (C1 & C2)[T]) =>
     source(type, c) ?? fallback(type, c);
 }
 
-export function fromRecord<R extends Record<AnyKey, any>, NotFound = never>(
+export function fromRecord<R extends Record<AnyKey, any>>(
   record: R
-): Resolver<{ [K in keyof R]: any }, R, NotFound> {
+): Resolver<{ [K in keyof R]: any }, R> {
   return (type) => record[type];
+}
+
+export function overrideByRecord<
+  C extends Record<AnyKey, any>,
+  R extends Record<AnyKey, any>,
+  O extends Record<AnyKey, any>,
+>(resolver: Resolver<C, R>, override: O) {
+  return chain(fromRecord(override), resolver);
 }
