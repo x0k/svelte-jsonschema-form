81cc3fc9384430ebe3d23cee17980c03f8b3f369
Roman Krasilnikov <x0kill@ya.ru>
2025-03-11 16:17:38 +0300

    [fields] Add type definition for ExtendableComponentDefinitions without existing fields

--- packages/form/src/fields/resolver/index.ts
+++ packages/form/src/fields/resolver/index.ts
@@ -1,7 +1,13 @@
 import { fromRecord } from "@/lib/resolver.js";
+import type { ExtendableComponentDefinitions } from "@/form/index.js";
 
 import "../exports.js";
 
 import { definitions } from "./definitions.js";
 
 export const fields = fromRecord(definitions);
+
+export type ExtendableComponentDefinitionsWithoutFields = Omit<
+  ExtendableComponentDefinitions,
+  keyof typeof definitions
+>;
