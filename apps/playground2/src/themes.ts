3388ac60f7a5bd4773bb8c3e9589f36e570f1360
Roman Krasilnikov <x0kill@ya.ru>
2025-03-13 10:12:14 +0300

    [playground] Add extra widget imports

--- apps/playground2/src/themes.ts
+++ apps/playground2/src/themes.ts
@@ -5,6 +5,8 @@ import "@sjsf/form/fields/extra-fields/files-include";
 import "@sjsf/form/fields/extra-fields/multi-enum-include";
 
 import { theme as basic } from "@sjsf/basic-theme";
+import '@sjsf/basic-theme/extra-widgets/checkboxes-include';
+import '@sjsf/basic-theme/extra-widgets/file-include';
 import "@sjsf/basic-theme/extra-widgets/multi-select-include";
 import "@sjsf/basic-theme/extra-widgets/textarea-include";
 import "@sjsf/basic-theme/extra-widgets/radio-include";
@@ -12,6 +14,8 @@ import "@sjsf/basic-theme/extra-widgets/date-picker-include";
 
 import { theme as daisy } from "@sjsf/daisyui-theme";
 import daisyStyles from "@sjsf/daisyui-theme/styles.css?inline";
+import '@sjsf/daisyui-theme/extra-widgets/checkboxes-include';
+import '@sjsf/daisyui-theme/extra-widgets/file-include';
 import "@sjsf/daisyui-theme/extra-widgets/multi-select-include";
 import "@sjsf/daisyui-theme/extra-widgets/textarea-include";
 import "@sjsf/daisyui-theme/extra-widgets/radio-include";
@@ -19,6 +23,8 @@ import "@sjsf/daisyui-theme/extra-widgets/date-picker-include";
 
 import { theme as daisy5 } from "@sjsf/daisyui5-theme";
 import daisy5Styles from "@sjsf/daisyui5-theme/styles.css?raw";
+import '@sjsf/daisyui5-theme/extra-widgets/checkboxes-include';
+import '@sjsf/daisyui5-theme/extra-widgets/file-include';
 import "@sjsf/daisyui5-theme/extra-widgets/multi-select-include";
 import "@sjsf/daisyui5-theme/extra-widgets/textarea-include";
 import "@sjsf/daisyui5-theme/extra-widgets/radio-include";
@@ -26,6 +32,8 @@ import "@sjsf/daisyui5-theme/extra-widgets/cally-date-picker-include";
 
 import { theme as flowbite } from "@sjsf/flowbite-theme";
 import flowbiteStyles from "@sjsf/flowbite-theme/styles.css?inline";
+import '@sjsf/flowbite-theme/extra-widgets/checkboxes-include';
+import '@sjsf/flowbite-theme/extra-widgets/file-include';
 import "@sjsf/flowbite-theme/extra-widgets/multi-select-include";
 import "@sjsf/flowbite-theme/extra-widgets/textarea-include";
 import "@sjsf/flowbite-theme/extra-widgets/radio-include";
@@ -33,6 +41,8 @@ import "@sjsf/flowbite-theme/extra-widgets/date-picker-include";
 
 import { theme as skeleton } from "@sjsf/skeleton-theme";
 import skeletonStyles from "@sjsf/skeleton-theme/styles.css?inline";
+import '@sjsf/skeleton-theme/extra-widgets/checkboxes-include';
+import '@sjsf/skeleton-theme/extra-widgets/file-include';
 import "@sjsf/skeleton-theme/extra-widgets/multi-select-include";
 import "@sjsf/skeleton-theme/extra-widgets/textarea-include";
 import "@sjsf/skeleton-theme/extra-widgets/radio-include";
@@ -40,6 +50,8 @@ import "@sjsf/skeleton-theme/extra-widgets/date-picker-include";
 
 import { theme as shadcn } from "@sjsf/shadcn-theme";
 import shadcnStyles from "@sjsf/shadcn-theme/styles.css?inline";
+import '@sjsf/shadcn-theme/extra-widgets/checkboxes-include';
+import '@sjsf/shadcn-theme/extra-widgets/file-include';
 import "@sjsf/shadcn-theme/extra-widgets/multi-select-include";
 import "@sjsf/shadcn-theme/extra-widgets/textarea-include";
 import "@sjsf/shadcn-theme/extra-widgets/radio-include";
