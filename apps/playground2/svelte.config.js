328b9c7755ade55c3534f5d61e2d9c37f0427928
Roman Krasilnikov <x0kill@ya.ru>
2025-03-17 01:38:07 +0300

    [playground] update dynamic compile options for nonRunic packages

--- apps/playground2/svelte.config.js
+++ apps/playground2/svelte.config.js
@@ -1,12 +1,17 @@
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
+const nonRunic = ["lucide-svelte", "flowbite-theme", "flowbite-svelte"];
+
 export default {
   // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
   // for more information about preprocessors
   preprocess: vitePreprocess(),
-  compilerOptions: {
-    // Because `lucide-icons`
-    // runes: true,
-    css: 'injected'
+  vitePlugin: {
+    dynamicCompileOptions({ filename }) {
+      return {
+        runes: !nonRunic.some((p) => filename.includes(p)),
+        css: filename.includes("moving-icons") ? "injected" : "external",
+      };
+    },
   },
 };
