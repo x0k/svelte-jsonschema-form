f37bf4a9e49eec84bb64b8471ff8f34bc056fdd8
Roman Krasilnikov <x0kill@ya.ru>
2025-03-16 15:01:11 +0300

    [form] Return `getValueByPath` function

--- packages/form/src/lib/object.ts
+++ packages/form/src/lib/object.ts
@@ -5,3 +5,27 @@ export function isObject(value: unknown): value is object {
 export function isRecord<T>(value: unknown): value is Record<PropertyKey, T> {
   return isObject(value) && !Array.isArray(value);
 }
+
+export function getValueByPath<T, R>(
+  from: T,
+  path: PropertyKey[],
+  defaultValue?: R
+): R | undefined {
+  let result = from as unknown;
+  for (const k of path) {
+    if (
+      Array.isArray(result) &&
+      typeof k === "number" &&
+      k >= 0 &&
+      k < result.length
+    ) {
+      result = result[k];
+      continue;
+    } else if (isRecord(result) && k in result) {
+      result = result[k];
+      continue;
+    }
+    return defaultValue;
+  }
+  return result as R;
+}
