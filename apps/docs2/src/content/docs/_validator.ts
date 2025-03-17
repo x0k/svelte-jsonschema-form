c376bcb336b3432f2b97e645a0f1bbcd8006fe18
Roman Krasilnikov <x0kill@ya.ru>
2025-03-17 17:45:44 +0300

    [docs] Simplify validator code

--- apps/docs2/src/content/docs/_validator.ts
+++ apps/docs2/src/content/docs/_validator.ts
@@ -1,34 +1,27 @@
 import type { ErrorObject } from "ajv";
 import { createFormValidator } from "@sjsf/ajv8-validator";
 import { isSchemaObjectValue } from "@sjsf/form/core";
-import {
-  pathToId,
-  type FormValueValidator,
-  type ValidationError,
-} from "@sjsf/form";
+import { pathToId, type FormValueValidator } from "@sjsf/form";
 
 class StarError {}
 
-function createStarValidator<V extends FormValueValidator<ErrorObject>>(
-  v: V,
-  idPrefix: string
-) {
+export function createValidator(idPrefix: string) {
+  const validator = createFormValidator({ idPrefix });
   return {
-    ...v,
+    ...validator,
     validateFormValue(rootSchema, formValue) {
-      const errors: ValidationError<ErrorObject | StarError>[] =
-        v.validateFormValue(rootSchema, formValue);
+      const errors = validator.validateFormValue(rootSchema, formValue);
       return isSchemaObjectValue(formValue) && !formValue["star"]
-        ? errors.concat({
-            instanceId: pathToId(["star"], { idPrefix }),
-            propertyTitle: "Star",
-            message: "That's okay, but I would appreciate your support!",
-            error: new StarError(),
-          })
+        ? [
+            ...errors,
+            {
+              instanceId: pathToId(["star"], { idPrefix }),
+              propertyTitle: "Star",
+              message: "That's okay, but I would appreciate your support!",
+              error: new StarError(),
+            },
+          ]
         : errors;
     },
   } satisfies FormValueValidator<ErrorObject | StarError>;
 }
-
-export const createValidator = (idPrefix: string) =>
-  createStarValidator(createFormValidator({ idPrefix }), idPrefix);
