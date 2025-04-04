b5277019d0415e258e133e98030c8b81e5d5ccb1
Roman Krasilnikov <x0kill@ya.ru>
2025-04-04 10:32:59 +0300

    [docs] Update form component references from CustomForm to MyForm

--- apps/docs2/src/content/docs/guides/_multiple-forms.svelte
+++ apps/docs2/src/content/docs/guides/_multiple-forms.svelte
@@ -2,31 +2,30 @@
   import type { Schema } from "@sjsf/form";
   import { createFormValidator } from "@sjsf/ajv8-validator";
 
-  import CustomForm from "@/components/my-form.svelte";
+  import MyForm from "@/components/my-form.svelte";
 
   const schema: Schema = {
     type: "string",
   };
-  const onSubmit = (v: string) => window.alert(v);
 </script>
 
 <div style="display: flex; gap: 1rem; justify-content: space-around;">
-  <CustomForm
+  <MyForm
     {schema}
-    {onSubmit}
     initialValue="foo"
     idPrefix="form1"
     validator={createFormValidator({
       idPrefix: "form1",
     })}
+    onSubmit={(v: string) => window.alert(v)}
   />
-  <CustomForm
+  <MyForm
     {schema}
-    {onSubmit}
     initialValue="bar"
     idPrefix="form2"
     validator={createFormValidator({
       idPrefix: "form2",
     })}
+    onSubmit={(v: string) => window.alert(v)}
   />
 </div>
