61b8daba452f2490457a2b6f202c81276705efc4
Roman Krasilnikov <x0kill@ya.ru>
2025-04-08 00:24:05 +0300

    [lib] Add `fromFactories` function to `lib/resolver`

---
+++ .changeset/big-women-follow.md
@@ -0,0 +1,5 @@
+---
+"@sjsf/form": minor
+---
+
+Add `fromFactories` function to `lib/resolver`
--- packages/form/src/lib/resolver.ts
+++ packages/form/src/lib/resolver.ts
@@ -1,27 +1,33 @@
 export type Resolved<
   T extends PropertyKey,
-  R extends Record<PropertyKey, any>,
+  R extends Record<PropertyKey, any>
 > = R[T] extends never ? undefined : R[T];
 
 export type Resolver<
   C extends Record<PropertyKey, any>,
-  R extends Record<PropertyKey, any>,
+  R extends Record<PropertyKey, any>
 > = {
   <T extends keyof C>(type: T, config: C[T]): Resolved<T, R>;
   __configs?: C;
   __results?: R;
 };
 
-export type ResolverConfigs<R extends Resolver<any, any>> = Exclude<R["__configs"], undefined>;
-export type ResolverResults<R extends Resolver<any, any>> = Exclude<R["__results"], undefined>;
+export type ResolverConfigs<R extends Resolver<any, any>> = Exclude<
+  R["__configs"],
+  undefined
+>;
+export type ResolverResults<R extends Resolver<any, any>> = Exclude<
+  R["__results"],
+  undefined
+>;
 export type Chain<
   R1 extends Record<PropertyKey, any>,
-  R2 extends Record<PropertyKey, any>,
+  R2 extends Record<PropertyKey, any>
 > = R1 & Omit<R2, keyof R1>;
 
 export function chain<
   R1 extends Resolver<any, any>,
-  R2 extends Resolver<any, any>,
+  R2 extends Resolver<any, any>
 >(
   source: R1,
   fallback: R2
@@ -41,16 +47,31 @@ export function fromRecord<R extends Record<PropertyKey, any>>(
   return (type) => record[type];
 }
 
+export function fromFactories<
+  R extends Record<PropertyKey, (config: any) => any>
+>(
+  factories: R
+): Resolver<
+  {
+    [K in keyof R]: Parameters<R[K]>[0];
+  },
+  {
+    [K in keyof R]: ReturnType<R[K]>;
+  }
+> {
+  return (type, config) => factories[type]?.(config);
+}
+
 export function overrideByRecord<
   R extends Resolver<any, any>,
-  O extends Partial<ResolverResults<R>>,
+  O extends Partial<ResolverResults<R>>
 >(resolver: R, override: O) {
   return chain(fromRecord(override), resolver);
 }
 
 export function extendByRecord<
   R extends Resolver<any, any>,
-  E extends Record<PropertyKey, any>,
+  E extends Record<PropertyKey, any>
 >(resolver: R, extension: E) {
   return chain(resolver, fromRecord(extension));
 }
