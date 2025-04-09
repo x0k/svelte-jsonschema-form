c071c9c5d3150601f31a64c7a8a87052e29b7cc3
Roman Krasilnikov <x0kill@ya.ru>
2025-04-09 14:53:13 +0300

    [docs] Fix skeleton3 theme demo

--- apps/docs2/src/content/docs/themes/_skeleton3-theme.svelte
+++ apps/docs2/src/content/docs/themes/_skeleton3-theme.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
-  import { BasicForm } from '@sjsf/form';
+  import { fromRecord } from "@sjsf/form/lib/resolver";
+  import { BasicForm } from "@sjsf/form";
   import { theme } from "@sjsf/skeleton3-theme";
-  import { specs } from '@sjsf/skeleton3-theme/specs'
+  import { specs } from "@sjsf/skeleton3-theme/specs";
 
   import { createAstro } from "@/astro.svelte";
   import { createMyForm } from "@/components/my-form";
@@ -10,18 +11,42 @@
 
   const astro = createAstro();
 
+  let rootNode = $state<Node>();
+  const options = {
+    getRootNode() {
+      return rootNode!;
+    },
+  };
+
   const form = createMyForm({
     ...createSchemas(specs),
     theme,
+    extraUiOptions: fromRecord({
+      skeleton3Slider: options,
+      skeleton3FileUpload: options,
+      skeleton3Rating: options,
+      skeleton3Segment: options,
+      skeleton3Switch: options,
+      skeleton3Tags: options,
+    }),
+  });
+
+  let divEl: HTMLElement;
+  $effect(() => {
+    rootNode = divEl.getRootNode();
   });
 </script>
 
-<BasicForm
-  {form}
-  novalidate
-  class="flex flex-col gap-4 {astro.darkOrLight}"
-  style="margin-bottom: 1rem;"
-  data-theme="cerberus"
-/>
+<div bind:this={divEl}></div>
+
+{#if rootNode}
+  <BasicForm
+    {form}
+    novalidate
+    class="flex flex-col gap-4 {astro.darkOrLight}"
+    style="margin-bottom: 1rem;"
+    data-theme="cerberus"
+  />
+{/if}
 
 <pre>{JSON.stringify(form.value, null, 2)}</pre>
