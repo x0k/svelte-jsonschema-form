9e6890868c1b616f57dcd2c9335ba1b0a9e7953d
Roman Krasilnikov <x0kill@ya.ru>
2025-04-13 01:25:58 +0300

    [docs] Fix tabbed layout

--- apps/docs2/src/content/docs/examples/tabs/layout.svelte
+++ apps/docs2/src/content/docs/examples/tabs/layout.svelte
@@ -1,17 +1,22 @@
 <script lang="ts">
   import type { ComponentProps } from "@sjsf/form";
+  import { getArrayContext } from "@sjsf/form/fields/array/context.svelte";
   import Layout from "@sjsf/basic-theme/components/layout.svelte";
 
   import Tabs from "./tabs.svelte";
   import Tab from "./tab.svelte";
 
   const props: ComponentProps["layout"] = $props();
+
+  const arrCtx = getArrayContext();
+
+  const isTuple = $derived(arrCtx.fieldType === "tupleField");
 </script>
 
-{#if props.type === "array-items" && Array.isArray(props.config.schema.items)}
+{#if props.type === "array-items" && isTuple}
   <Tabs {...props} />
-{:else if props.type === "array-item" && props.config.required}
+{:else if props.type === "array-item" && isTuple}
   <Tab {...props} />
-{:else if !(props.type === "array-field-meta" && props.config.required)}
+{:else if !(props.type === "array-field-meta" && isTuple)}
   <Layout {...props} />
 {/if}
