535396a374b04160ea3800c1e44510c8fed4d9a4
Roman Krasilnikov <x0kill@ya.ru>
2025-04-13 01:58:42 +0300

    [docs] Add animated array example

---
+++ apps/docs2/src/content/docs/examples/animated/animated-array.svelte
@@ -0,0 +1,63 @@
+<script lang="ts">
+  import { flip } from "svelte/animate";
+  import { fade } from "svelte/transition";
+
+  import { isSchemaArrayValue } from "@sjsf/form/core";
+  import {
+    getComponent,
+    getFormContext,
+    type ComponentProps,
+    Text,
+  } from "@sjsf/form";
+  import {
+    createArrayContext,
+    setArrayContext,
+  } from "@sjsf/form/fields/array/context.svelte";
+
+  let { value = $bindable(), config }: ComponentProps["arrayField"] = $props();
+
+  const ctx = getFormContext();
+  const arrayCtx = createArrayContext(
+    ctx,
+    () => config,
+    () => value,
+    (v) => (value = v)
+  );
+  setArrayContext(arrayCtx);
+
+  const ArrayItem = $derived(getComponent(ctx, "arrayItemField", config));
+  const Template = $derived(getComponent(ctx, "arrayTemplate", config));
+  const Button = $derived(getComponent(ctx, "button", config));
+</script>
+
+{#snippet addButton()}
+  <Button
+    errors={arrayCtx.errors}
+    {config}
+    disabled={false}
+    type="array-item-add"
+    onclick={arrayCtx.pushItem}
+  >
+    <Text {config} id="add-array-item" />
+  </Button>
+{/snippet}
+<Template
+  type="template"
+  errors={arrayCtx.errors}
+  {config}
+  {value}
+  addButton={arrayCtx.canAdd() ? addButton : undefined}
+>
+  {#if isSchemaArrayValue(value)}
+    {#each value as item, index (arrayCtx.key(index))}
+      <div animate:flip transition:fade>
+        <ArrayItem
+          type="field"
+          {index}
+          config={arrayCtx.itemConfig(config, item, index)}
+          bind:value={value[index]}
+        />
+      </div>
+    {/each}
+  {/if}
+</Template>
