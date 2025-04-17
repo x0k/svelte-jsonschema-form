9678e5ba4fc5bde9be42dd87864cf3eabf4af8c6
Roman Krasilnikov <x0kill@ya.ru>
2025-04-17 18:09:23 +0300

    [playground] Add `schemasafe` validator

--- apps/playground2/src/validators.ts
+++ apps/playground2/src/validators.ts
@@ -1,7 +1,9 @@
 import { createFormValidator as ajv8 } from "@sjsf/ajv8-validator";
 import { createFormValidator as cfworker } from "@sjsf/cfworker-validator";
+import { createFormValidator as schemasafe } from "@sjsf/schemasafe-validator";
 
 export const validators = {
   ajv8,
   cfworker,
+  schemasafe,
 };
