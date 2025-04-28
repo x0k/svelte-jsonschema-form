1cdc29d821e4adcabbf85a6a9b967b22319265cd
Roman Krasilnikov <x0kill@ya.ru>
2025-04-28 18:53:54 +0300

    [docs] Add markdown description example

---
+++ apps/docs2/src/content/docs/examples/markdown/description.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import Markdown from "svelte-exmarkdown";
+
+  import type { ComponentProps } from "@sjsf/form";
+
+  const { description }: ComponentProps["description"] = $props();
+</script>
+
+<Markdown md={description}>
+  {#snippet p({ children })}
+    <div>{@render children?.()}</div>
+  {/snippet}
+</Markdown>
