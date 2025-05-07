a40ec91789098c632c2cde9b2d69595a13f13124
Roman Krasilnikov <x0kill@ya.ru>
2025-05-07 13:16:22 +0300

    [core] Comment out unused 'bad discriminator' cases

--- packages/form/src/core/discriminator.test.ts
+++ packages/form/src/core/discriminator.test.ts
@@ -18,21 +18,21 @@ describe('getDiscriminatorFieldFromSchema()', () => {
   it('returns the propertyName when discriminator is present', () => {
     expect(getDiscriminatorFieldFromSchema(GOOD_DISCRIMINATOR)).toEqual(PROPERTY_NAME);
   });
-  describe('bad discriminator', () => {
-    let consoleWarnSpy: MockInstance<typeof console.warn>;
-    beforeAll(() => {
-      consoleWarnSpy = vi.spyOn(console, "warn").mockImplementation(() => {}); // mock this to avoid actually warning in the tests
-    });
-    afterAll(() => {
-      consoleWarnSpy.mockRestore();
-    });
-    // it('returns undefined when discriminator is present, but not a string', () => {
-    //   expect(getDiscriminatorFieldFromSchema(BAD_DISCRIMINATOR)).toBeUndefined();
-    // });
-    // it('it also warns about the bad discriminator', () => {
-    //   expect(consoleWarnSpy).toHaveBeenCalledWith('Expecting discriminator to be a string, got "number" instead');
-    // });
-  });
+  // describe('bad discriminator', () => {
+  //   let consoleWarnSpy: MockInstance<typeof console.warn>;
+  //   beforeAll(() => {
+  //     consoleWarnSpy = vi.spyOn(console, "warn").mockImplementation(() => {}); // mock this to avoid actually warning in the tests
+  //   });
+  //   afterAll(() => {
+  //     consoleWarnSpy.mockRestore();
+  //   });
+  //   it('returns undefined when discriminator is present, but not a string', () => {
+  //     expect(getDiscriminatorFieldFromSchema(BAD_DISCRIMINATOR)).toBeUndefined();
+  //   });
+  //   it('it also warns about the bad discriminator', () => {
+  //     expect(consoleWarnSpy).toHaveBeenCalledWith('Expecting discriminator to be a string, got "number" instead');
+  //   });
+  // });
 });
 
 describe('getOptionMatchingSimpleDiscriminator()', () => {
