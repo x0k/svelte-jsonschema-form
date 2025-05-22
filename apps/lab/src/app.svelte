70bd9ac206784c502af28d939257824cef75088d
Roman Krasilnikov <x0kill@ya.ru>
2025-05-22 10:40:49 +0300

    [lab] Use `initUserConfiguration`

--- apps/lab/src/app.svelte
+++ apps/lab/src/app.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
-  import Editor from "@/components/editor.svelte";
-
-  import { createEditorState } from "./editor.svelte.js";
+  import { initUserConfiguration } from "@codingame/monaco-vscode-configuration-service-override";
   import { Pane, PaneGroup, PaneResizer } from "paneforge";
-  import * as monaco from "monaco-editor";
+  import type * as monaco from "monaco-editor";
+
+  import type { EditorState } from "./editor.svelte.js";
   import { themeManager } from "./theme.svelte.js";
   import Dropdown from "./components/dropdown.svelte";
   import { THEME_TITLES, THEMES } from "./shared.js";
@@ -15,7 +15,25 @@
   const editorTheme = $derived(
     themeManager.isDark ? "Default Dark Modern" : "Default Light Modern"
   );
-  const editorState = createEditorState();
+  // svelte-ignore non_reactive_update
+  let editorState: EditorState;
+
+  const editorPromise = initUserConfiguration(
+    JSON.stringify({
+      // svelte-ignore state_referenced_locally
+      "workbench.colorTheme": editorTheme,
+    })
+  )
+    .then(() =>
+      Promise.all([
+        import("@/components/editor.svelte"),
+        import("./editor.svelte.js"),
+      ])
+    )
+    .then(([c, m]) => {
+      editorState = m.createEditorState();
+      return c.default;
+    });
 </script>
 
 <svelte:window onresize={editorResize} />
@@ -36,14 +54,16 @@
     direction="horizontal"
     class="flex h-full w-full data-[direction=vertical]:flex-col"
   >
-    <Pane defaultSize={50} onResize={editorResize}
-      ><Editor
-        bind:editor
-        theme={editorTheme}
-        class="h-full w-full"
-        model={editorState.activeTab.model}
-      /></Pane
-    >
+    <Pane defaultSize={50} onResize={editorResize}>
+      {#await editorPromise then Editor}
+        <Editor
+          bind:editor
+          theme={editorTheme}
+          class="h-full w-full"
+          model={editorState.activeTab.model}
+        />
+      {/await}
+    </Pane>
     <PaneResizer
       class="bg-secondary/20 focus-visible:ring-ring relative flex w-px items-center justify-center after:absolute after:inset-y-0 after:left-1/2 after:w-1 after:-translate-x-1/2 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-offset-1 data-[direction=vertical]:h-px data-[direction=vertical]:w-full data-[direction=vertical]:after:left-0 data-[direction=vertical]:after:h-1 data-[direction=vertical]:after:w-full data-[direction=vertical]:after:-translate-y-1/2 data-[direction=vertical]:after:translate-x-0 [&[data-direction=vertical]>div]:rotate-90"
     />
