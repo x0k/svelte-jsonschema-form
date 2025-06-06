cf7ccb6220e66ab828dc6cbccf7bb29d3c603967
Roman Krasilnikov <x0kill@ya.ru>
2025-06-07 01:33:48 +0300

    [build] Cleanup vitest config

--- vitest.workspace.ts
+++ vitest.workspace.ts
@@ -3,18 +3,14 @@ import { defineWorkspace } from 'vitest/config'
 export default defineWorkspace([
   "./packages/form/vite.config.ts",
   "./packages/ajv8-validator/vitest.config.ts",
-  "./packages/shadcn-theme/vite.config.ts",
-  "./packages/skeleton-theme/vite.config.ts",
   "./packages/schemasafe-validator/vitest.config.ts",
   "./packages/sveltekit/vite.config.ts",
   "./packages/skeleton3-theme/vite.config.ts",
   "./packages/flowbite-icons/vite.config.ts",
   "./packages/lucide-icons/vite.config.ts",
   "./packages/testing/vite.config.ts",
-  "./packages/daisyui-theme/vite.config.ts",
   "./packages/flowbite3-theme/vite.config.ts",
   "./packages/radix-icons/vite.config.ts",
-  "./packages/flowbite-theme/vite.config.ts",
   "./packages/basic-theme/vite.config.ts",
   "./packages/moving-icons/vite.config.ts"
 ])
