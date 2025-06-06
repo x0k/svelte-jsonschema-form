7acae82e5b39154753d5e9b18bbdbb0b5156fdd8
Roman Krasilnikov <x0kill@ya.ru>
2025-06-06 11:47:32 +0300

    [build] Remove skeleton RC1 theme

--- packages/skeleton-theme/src/lib/widgets/exports.ts
+++
@@ -1,4 +0,0 @@
-export { default as textWidget } from './text.svelte';
-export { default as numberWidget } from './number.svelte';
-export { default as selectWidget } from './select.svelte';
-export { default as checkboxWidget } from './checkbox.svelte';
