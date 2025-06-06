2ba57e7554cdcba9bf3b4844b19ca868cefd8421
Roman Krasilnikov <x0kill@ya.ru>
2025-06-07 02:22:47 +0300

    [build] Add shadcn4 to vitest config

--- vitest.workspace.ts
+++ vitest.workspace.ts
@@ -5,6 +5,7 @@ export default defineWorkspace([
   "./packages/ajv8-validator/vitest.config.ts",
   "./packages/schemasafe-validator/vitest.config.ts",
   "./packages/sveltekit/vite.config.ts",
+  "./packages/shadcn4-theme/vite.config.ts",
   "./packages/skeleton3-theme/vite.config.ts",
   "./packages/flowbite-icons/vite.config.ts",
   "./packages/lucide-icons/vite.config.ts",
