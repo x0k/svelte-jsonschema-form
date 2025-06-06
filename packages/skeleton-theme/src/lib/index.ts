85eca13348be16cc481f3b6ce9ca2a32d4378f3d
Roman Krasilnikov <x0kill@ya.ru>
2025-06-06 11:47:32 +0300

    [build] Remove skeleton RC1 theme

--- packages/skeleton-theme/src/lib/index.ts
+++
@@ -1,6 +0,0 @@
-import { extendByRecord } from '@sjsf/form/lib/resolver';
-import { fields } from '@sjsf/form/fields/resolver';
-
-import { definitions } from './definitions';
-
-export const theme = extendByRecord(fields, definitions);
