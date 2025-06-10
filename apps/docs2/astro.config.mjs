84c0458b12971e7518ef178f00b1da7c59694875
Roman Krasilnikov <x0kill@ya.ru>
2025-06-10 22:40:42 +0300

    [docs] Add badges for new sections

--- apps/docs2/astro.config.mjs
+++ apps/docs2/astro.config.mjs
@@ -47,6 +47,7 @@ export default defineConfig({
         {
           // label: "Examples",
           slug: "examples",
+          badge: "new",
         },
         // {
         //   label: "Examples",
