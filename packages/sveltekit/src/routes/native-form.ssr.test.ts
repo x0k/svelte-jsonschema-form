eb8dfcda7e6d5ad6036d12aa0e8800aec79d48eb
Roman Krasilnikov <x0kill@ya.ru>
2025-06-21 01:56:59 +0300

    [sveltekit] Refactor SSR tests for native form

--- packages/sveltekit/src/routes/native-form.ssr.test.ts
+++ packages/sveltekit/src/routes/native-form.ssr.test.ts
@@ -1,14 +1,17 @@
-import { initForm } from '$lib/server/server.js';
-import type { Page } from '@sveltejs/kit';
 import { render } from 'svelte/server';
+import type { Page } from '@sveltejs/kit';
 import { beforeEach, describe, expect, it, vi } from 'vitest';
+import { pathToId } from '@sjsf/form';
 
-const form = () => import('./native-form.svelte').then((m) => m.default);
-function mock(page: Partial<Page>) {
+import type { ValidatedFormData } from '$lib/model.js';
+import { initForm } from '$lib/server/server.js';
+
+async function renderForm(page: Partial<Page>) {
   vi.doMock('$app/state', () => ({
     page
   }));
-  return form();
+  const m = await import('./native-form.svelte');
+  return render(m.default);
 }
 
 describe('native form SSR', () => {
@@ -16,18 +19,100 @@ describe('native form SSR', () => {
     vi.resetModules();
     vi.clearAllMocks();
   });
-  it('should render simple form', async () => {
-    const Form = await mock({
+  it('should render initial values', async () => {
+    const { body } = await renderForm({
       data: {
         form: initForm({
           sendSchema: true,
           schema: { title: 'Schema title', type: 'string' },
-          initialValue: 'initial value'
+          initialValue: 'initial value',
+          initialErrors: [
+            {
+              instanceId: pathToId([]),
+              propertyTitle: 'Property',
+              message: 'error message',
+              error: null
+            }
+          ]
         })
       }
     });
-    const { body } = render(Form);
     expect(body).toContain('Schema title');
     expect(body).toContain('initial value');
+    expect(body).toContain('error message');
+  });
+  it('should render action result', async () => {
+    const { body } = await renderForm({
+      data: {
+        form: initForm({
+          sendSchema: true,
+          schema: { title: 'Schema title', type: 'string' },
+          initialValue: 'initial value',
+          initialErrors: [
+            {
+              instanceId: pathToId([]),
+              propertyTitle: 'Property',
+              message: 'error message',
+              error: null
+            }
+          ]
+        })
+      },
+      form: {
+        form: {
+          isValid: false,
+          sendData: true,
+          data: 'validated value',
+          errors: [
+            {
+              instanceId: pathToId([]),
+              propertyTitle: 'Validated property',
+              message: 'validation error message',
+              error: null
+            }
+          ]
+        } satisfies ValidatedFormData<null, true>
+      }
+    });
+    expect(body).toContain('Schema title');
+    expect(body).toContain('validated value');
+    expect(body).toContain('validation error message');
+  });
+  it('should display the initial values if the validation was successful', async () => {
+    const { body } = await renderForm({
+      data: {
+        form: initForm({
+          sendSchema: true,
+          schema: { title: 'Schema title', type: 'string' },
+          initialValue: 'initial value',
+          initialErrors: [
+            {
+              instanceId: pathToId([]),
+              propertyTitle: 'Property',
+              message: 'error message',
+              error: null
+            }
+          ]
+        })
+      },
+      form: {
+        form: {
+          isValid: true,
+          sendData: true,
+          data: 'validated value',
+          errors: [
+            {
+              instanceId: pathToId([]),
+              propertyTitle: 'Validated property',
+              message: 'validation error message',
+              error: null
+            }
+          ]
+        } satisfies ValidatedFormData<null, true>
+      }
+    });
+    expect(body).toContain('Schema title');
+    expect(body).toContain('initial value');
+    expect(body).toContain('error message');
   });
 });
