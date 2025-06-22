00a784f1c47639c52febc97fe675cbb42c948681
x0k <x0k@users.noreply.github.com>
2025-06-22 20:01:35 +0000

    deploy: d3c514f85f04ee4fbcd24812f18cd11cd964045f

--- _astro/legacy.DaW1F2nJ.js
+++
@@ -1,1 +0,0 @@
-import{B as a}from"./render.BV3w21Ef.js";a();
---
+++ _astro/model.DhnH2-Tv.js
@@ -0,0 +1,1 @@
+import"./definitions.Dt-LkhWU.js";import{g as o}from"./path.oE8Qtd-m.js";function a(t,i){const e=o(t,t,i);return typeof e=="object"?e.title:void 0}export{a as g};
