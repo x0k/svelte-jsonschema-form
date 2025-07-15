578c7fb0c67510dcc159d0203d10f6b02ff9a45f
Roman Krasilnikov <x0kill@ya.ru>
2025-07-15 17:27:47 +0300

    [builder] Fix skeleton shadow dom support

--- apps/builder/src/builder/preview/form.svelte
+++ apps/builder/src/builder/preview/form.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import { fromRecord } from "@sjsf/form/lib/resolver";
   import { BasicForm, createForm } from "@sjsf/form";
   import { BitsConfig } from "bits-ui";
 
@@ -8,13 +9,22 @@
   import { SJSF_RESOLVERS } from "$lib/sjsf/resolver.js";
   import { ICONS_STYLES, SJSF_ICONS } from "$lib/sjsf/icons.js";
   import { SJSF_VALIDATORS } from "$lib/sjsf/validators.js";
+  import { highlight } from "$lib/shiki.js";
 
   import { themeManager } from "../../theme.svelte.js";
 
   import { getBuilderContext } from "../context.svelte.js";
-  import { highlight } from "$lib/shiki.js";
 
   const ctx = getBuilderContext();
+
+  let portalEl = $state.raw() as HTMLDivElement;
+  const rootNode = $derived(portalEl?.getRootNode());
+
+  const options = {
+    getRootNode() {
+      return rootNode;
+    },
+  };
 
   const form = createForm({
     ...defaults,
@@ -36,12 +46,18 @@
     get validator() {
       return SJSF_VALIDATORS[ctx.validator];
     },
+    extraUiOptions: fromRecord({
+      skeleton3Slider: options,
+      skeleton3FileUpload: options,
+      skeleton3Rating: options,
+      skeleton3Segment: options,
+      skeleton3Switch: options,
+      skeleton3Tags: options,
+    }),
     onSubmit: console.log,
     onSubmitError: console.warn,
     onSubmissionFailure: console.error,
   });
-
-  let portalEl = $state.raw() as HTMLDivElement;
 </script>
 
 <div class="flex flex-col gap-2">
@@ -49,19 +65,21 @@
     class="rounded border border-[var(--global-border)]"
     style={`${THEME_STYLES[ctx.theme]}\n${ICONS_STYLES[ctx.icons]}`}
   >
-    <BitsConfig defaultPortalTo={portalEl}>
-      <BasicForm
-        id="form"
-        {form}
-        novalidate={!ctx.html5Validation}
-        class={themeManager.darkOrLight}
-        style="padding: 1rem; display: flex; flex-direction: column; gap: 1rem;"
-        data-theme={ctx.theme.startsWith(Theme.Skeleton3)
-          ? "cerberus"
-          : themeManager.darkOrLight}
-      />
-      <div bind:this={portalEl}></div>
-    </BitsConfig>
+    {#if portalEl}
+      <BitsConfig defaultPortalTo={portalEl}>
+        <BasicForm
+          id="form"
+          {form}
+          novalidate={!ctx.html5Validation}
+          class={themeManager.darkOrLight}
+          style="padding: 1rem; display: flex; flex-direction: column; gap: 1rem;"
+          data-theme={ctx.theme.startsWith(Theme.Skeleton3)
+            ? "cerberus"
+            : themeManager.darkOrLight}
+        />
+      </BitsConfig>
+    {/if}
+    <div bind:this={portalEl}></div>
   </ShadowHost>
   {#if form.value !== undefined}
     <div class="rounded-md border">
