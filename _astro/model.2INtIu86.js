9069d6124f193cbeeb76b5135c9b1b2591471952
x0k <x0k@users.noreply.github.com>
2025-07-18 11:05:11 +0000

    deploy: 2d02abf539842d71b5ccaa2a301fa1ff6df0124a

---
+++ _astro/model.2INtIu86.js
@@ -0,0 +1,1 @@
+import"./definitions.Bgt0vOBR.js";import{g as o}from"./path.DY2tbDNs.js";function a(t,i){const e=o(t,t,i);return typeof e=="object"?e.title:void 0}export{a as g};
--- _astro/model.DYloE0Hb.js
+++
@@ -1,1 +0,0 @@
-import"./definitions.OqM1ZuYd.js";import{g as o}from"./path.tvJt5eeQ.js";function a(t,i){const e=o(t,t,i);return typeof e=="object"?e.title:void 0}export{a as g};
