7a574d9b087cb123b086eea760c9698095048ff9
Roman Krasilnikov <x0kill@ya.ru>
2025-07-19 14:48:49 +0300

    [docs] Add `valueRef` option

---
+++ apps/docs2/src/content/docs/guides/controlled-form.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+  import { BasicForm, createForm, type Schema } from "@sjsf/form";
+
+  import * as defaults from "@/components/form-defaults";
+
+  const schema: Schema = {
+    type: "string",
+    minLength: 10,
+  };
+
+  let value = $state("initial");
+
+  const form = createForm({
+    ...defaults,
+    schema,
+    valueRef: {
+      get current() {
+        return value;
+      },
+      set current(v) {
+        value = v;
+      },
+    },
+    onSubmit: console.log,
+  });
+</script>
+
+<BasicForm {form} novalidate />
+
+<pre>{JSON.stringify(
+    { value: form.value, errors: Object.fromEntries(form.errors) },
+    null,
+    2
+  )}</pre>
