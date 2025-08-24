19fa6c23d591d66907de1a3bd21f1e3525b3f080
Roman Krasilnikov <x0kill@ya.ru>
2025-08-24 21:55:55 +0300

    [testing] Add default merger factory

--- packages/testing/src/components/form-defaults.ts
+++ packages/testing/src/components/form-defaults.ts
@@ -1,7 +1,13 @@
-import type { Validator } from "@sjsf/form";
+import type { MergerFactoryOptions, Validator } from "@sjsf/form";
 export { translation } from "@sjsf/form/translations/en";
 export { resolver } from "@sjsf/form/resolvers/basic";
 
 import { createFormValidator } from "@sjsf/ajv8-validator";
+import { createFormMerger } from "@sjsf/form/mergers/modern";
 
 export const validator: Validator = createFormValidator();
+
+export const createMerger = <V extends Validator>({
+  schema,
+  validator,
+}: MergerFactoryOptions<V>) => createFormMerger(validator, schema);
