574fb02acb797b4a9e04f5d21d4c89e07e5952c8
Roman Krasilnikov <x0kill@ya.ru>
2025-08-25 23:10:57 +0300

    [testing] Use `createFormValidator` and `createFormMerger` directly

--- packages/testing/src/components/form-defaults.ts
+++ packages/testing/src/components/form-defaults.ts
@@ -1,17 +1,4 @@
-import type {
-  MergerFactoryOptions,
-  Validator,
-  ValidatorFactoryOptions,
-} from "@sjsf/form";
 export { translation } from "@sjsf/form/translations/en";
 export { resolver } from "@sjsf/form/resolvers/basic";
-import { createFormValidator } from "@sjsf/ajv8-validator";
-import { createFormMerger } from "@sjsf/form/mergers/modern";
-
-export const createValidator = (options: ValidatorFactoryOptions): Validator =>
-  createFormValidator(options);
-
-export const createMerger = <V extends Validator>({
-  schema,
-  validator,
-}: MergerFactoryOptions<V>) => createFormMerger(validator, schema);
+export { createFormValidator as createValidator } from "@sjsf/ajv8-validator";
+export { createFormMerger as createMerger } from "@sjsf/form/mergers/modern";
