1d2210c91e42f4fa4830d02a3482bfa7fe1f1e77
Roman Krasilnikov <x0kill@ya.ru>
2025-08-25 15:21:58 +0300

    [testing] Use validator factory instead of specific instance

--- packages/testing/src/components/form-defaults.ts
+++ packages/testing/src/components/form-defaults.ts
@@ -1,10 +1,15 @@
-import type { MergerFactoryOptions, Validator } from "@sjsf/form";
+import type {
+  MergerFactoryOptions,
+  Validator,
+  ValidatorFactoryOptions,
+} from "@sjsf/form";
 export { translation } from "@sjsf/form/translations/en";
 export { resolver } from "@sjsf/form/resolvers/basic";
 import { createFormValidator } from "@sjsf/ajv8-validator";
 import { createFormMerger } from "@sjsf/form/mergers/modern";
 
-export const validator: Validator = createFormValidator();
+export const createValidator = (options: ValidatorFactoryOptions): Validator =>
+  createFormValidator(options);
 
 export const createMerger = <V extends Validator>({
   schema,
