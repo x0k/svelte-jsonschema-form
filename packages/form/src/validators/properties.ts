94c5225084462f13c31781f90875d973c496ab88
Roman Krasilnikov <x0kill@ya.ru>
2025-09-07 00:18:05 +0300

    [form] Eliminate the use of deprecated code

--- packages/form/src/validators/properties.ts
+++ packages/form/src/validators/properties.ts
@@ -1,70 +1,58 @@
-import { weakMemoize } from "@/lib/memoize.js";
-import {
-  transformSchemaDefinition,
-  type Schema,
-  type SchemaDefinition,
-} from "@/core/index.js";
-import type { AdditionalPropertyKeyValidator, Config } from "@/form/main.js";
+import { weakMemoize } from '@/lib/memoize.js';
+import { transformSchemaDefinition } from '@/lib/json-schema/index.js';
+import type { Schema, SchemaDefinition } from '@/core/index.js';
+import type { AdditionalPropertyKeyValidator, Config } from '@/form/main.js';
 
 // This is an attempt to restore the original scheme for correct field validation
 // TODO: Remove in v3
 export function removeVirtualAdditionalProperties(schema: Schema): Schema {
-  return transformSchemaDefinition(schema, (copy: SchemaDefinition) => {
-    if (typeof copy === "boolean") {
-      return copy;
-    }
-    const { properties } = copy;
-    if (properties === undefined) {
-      return copy;
-    }
-    for (const key of Object.keys(properties)) {
-      const property = properties[key]!;
-      if (typeof property === "boolean") {
-        continue;
-      }
-      //@ts-expect-error
-      if (property[ADDITIONAL_PROPERTY_FLAG]) {
-        // NOTE: properties also a copy
-        delete properties[key];
-      }
-    }
-    return copy;
-  }) as Schema;
+	return transformSchemaDefinition(schema, (copy: SchemaDefinition) => {
+		if (typeof copy === 'boolean') {
+			return copy;
+		}
+		const { properties } = copy;
+		if (properties === undefined) {
+			return copy;
+		}
+		for (const key of Object.keys(properties)) {
+			const property = properties[key]!;
+			if (typeof property === 'boolean') {
+				continue;
+			}
+			//@ts-expect-error
+			if (property[ADDITIONAL_PROPERTY_FLAG]) {
+				// NOTE: properties also a copy
+				delete properties[key];
+			}
+		}
+		return copy;
+	}) as Schema;
 }
 
-export function createVirtualAdditionalPropertiesRemover(
-  cache = new WeakMap<Config, Config>()
-) {
-  return weakMemoize(cache, (config) => ({
-    ...config,
-    schema: removeVirtualAdditionalProperties(config.schema),
-  }));
+export function createVirtualAdditionalPropertiesRemover(cache = new WeakMap<Config, Config>()) {
+	return weakMemoize(cache, (config) => ({
+		...config,
+		schema: removeVirtualAdditionalProperties(config.schema)
+	}));
 }
 
 export interface ErrorFactoryOptions {
-  key: string;
-  patternProperties: Exclude<Schema["patternProperties"], undefined>;
+	key: string;
+	patternProperties: Exclude<Schema['patternProperties'], undefined>;
 }
 
 export function createPatternPropertyKeyValidator(
-  error: string | ((ctx: ErrorFactoryOptions) => string)
+	error: string | ((ctx: ErrorFactoryOptions) => string)
 ): AdditionalPropertyKeyValidator {
-  return {
-    validateAdditionalPropertyKey(
-      key,
-      { patternProperties, additionalProperties }
-    ) {
-      let patterns: string[];
-      return patternProperties === undefined ||
-        additionalProperties !== false ||
-        ((patterns = Object.keys(patternProperties)), patterns.length === 0) ||
-        patterns.some((k) => new RegExp(k).test(key))
-        ? []
-        : [
-            typeof error === "string"
-              ? error
-              : error({ key, patternProperties }),
-          ];
-    },
-  };
+	return {
+		validateAdditionalPropertyKey(key, { patternProperties, additionalProperties }) {
+			let patterns: string[];
+			return patternProperties === undefined ||
+				additionalProperties !== false ||
+				((patterns = Object.keys(patternProperties)), patterns.length === 0) ||
+				patterns.some((k) => new RegExp(k).test(key))
+				? []
+				: [typeof error === 'string' ? error : error({ key, patternProperties })];
+		}
+	};
 }
