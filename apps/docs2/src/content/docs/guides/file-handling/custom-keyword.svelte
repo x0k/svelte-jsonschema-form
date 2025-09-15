54b32f9f4ad93d367ca1a3ffb5443a0bd0f99fa0
Roman Krasilnikov <x0kill@ya.ru>
2025-09-15 21:13:13 +0300

    [docs] Add files validation section

---
+++ apps/docs2/src/content/docs/guides/file-handling/custom-keyword.svelte
@@ -0,0 +1,56 @@
+<script lang="ts" module>
+  declare module "@sjsf/form" {
+    interface Schema {
+      maxSizeBytes?: number;
+    }
+  }
+</script>
+
+<script lang="ts">
+  import { type Ajv } from "ajv";
+  import { BasicForm, createForm, ON_CHANGE } from "@sjsf/form";
+  import { formatFileSize } from "@sjsf/form/validators/file-size";
+  import { addFormComponents } from "@sjsf/ajv8-validator";
+
+  import * as defaults from "@/lib/form/defaults";
+
+  function addKeywords(ajv: Ajv): Ajv {
+    ajv.addKeyword({
+      keyword: "maxSizeBytes",
+      validate(max: number, data: unknown) {
+        if (data === undefined) {
+          return true;
+        }
+        if (!(data instanceof File)) {
+          throw new Error(`Expected "File", but got "${typeof data}"`);
+        }
+        return data.size <= max;
+      },
+      error: {
+        message: (ctx) => `Max file size ${formatFileSize(ctx.schema)}`,
+      },
+    });
+    return ajv;
+  }
+
+  const form = createForm({
+    ...defaults,
+    createValidator: (options) =>
+      defaults.createValidator({
+        ...options,
+        ajvPlugins: (ajv) => addKeywords(addFormComponents(ajv)),
+      }),
+    schema: {
+      title: "File",
+      maxSizeBytes: 1024 * 4,
+    },
+    uiSchema: {
+      "ui:components": {
+        unknownField: "nativeFileFieldWrapper",
+      },
+    },
+    fieldsValidationMode: ON_CHANGE,
+  });
+</script>
+
+<BasicForm {form} />
