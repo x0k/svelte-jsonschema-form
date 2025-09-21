618a63298186c0b592045c782550cfb356dec496
Roman Krasilnikov <x0kill@ya.ru>
2025-09-22 02:03:44 +0300

    [docs] Update `createSchemas` function

--- apps/docs2/src/content/docs/themes/_demo-schema.ts
+++ apps/docs2/src/content/docs/themes/_demo-schema.ts
@@ -1,18 +1,6 @@
-import { cast } from "@sjsf/form/lib/component";
-import type { ComponentDefinition, Schema, UiSchema } from "@sjsf/form";
-import FilesField from "@sjsf/form/fields/extra-fields/files.svelte";
 import { s } from "testing/demo";
 
 import { THEME_PACKAGES, type Theme } from "@/shared";
-
-const filesAsArrayField = cast(FilesField, {
-  value: {
-    transform(props) {
-      s.assertStrings(props.value);
-      return props.value;
-    },
-  },
-}) satisfies ComponentDefinition<"arrayField">;
 
 const WIDGET_USED_AS_DEFAULT_IN_FIELDS: Record<string, string[] | undefined> = {
   checkboxes: ["multiEnumField"],
@@ -34,51 +22,11 @@ export function createExtraImports(theme: Theme, widgets: string[]) {
     .join("\n");
 }
 
-export function createSchemas(specs: Record<string, [Schema, UiSchema]> = {}) {
-  return s.createSchemas({
-    checkbox: [s.boolean, {}],
-    checkboxes: [
-      s.uniqueArray,
-      {
-        "ui:components": {
-          arrayField: "multiEnumField",
-        },
-      },
-    ],
-    file: [
-      s.file,
-      {
-        "ui:components": {
-          stringField: "fileField",
-        },
-      },
-    ],
-    multiFile: [
-      {
-        type: "array",
-        items: s.file,
-      },
-      {
-        "ui:components": {
-          arrayField: filesAsArrayField,
-        },
-      },
-    ],
-    number: [s.number, {}],
-    select: [
-      s.enumeration,
-      {
-        "ui:components": {
-          stringField: "enumField",
-        },
-      },
-    ],
-    text: [s.text, {}],
-    ...specs,
-  });
+export function createSchemas(specs: s.Specs = {}) {
+  return s.createSchemas(specs);
 }
 
-export function replacer(key: string, value: any) {
+export function replacer(_: string, value: any) {
   if (typeof value === "function") {
     return `<function: ${value.name || "anonymous"}>`;
   }
