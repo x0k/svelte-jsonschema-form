9b862cbe656b7eac88857729d9c88d4e6df21e61
Roman Krasilnikov <x0kill@ya.ru>
2025-09-22 15:54:13 +0300

    [build] Bump playwright version

--- flake.nix
+++ flake.nix
@@ -1,23 +1,28 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.05";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     mk.url = "github:x0k/mk";
   };
   outputs =
     {
       self,
       nixpkgs,
+      nixpkgs-unstable,
       mk,
     }:
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
+      unstablePkgs = import nixpkgs-unstable {
+        inherit system;
+      };
     in
     {
       devShells.${system} = {
         default = pkgs.mkShell {
           nativeBuildInputs = [
-            pkgs.playwright.browsers
+            unstablePkgs.playwright.browsers
           ];
           buildInputs = [
             mk.packages.${system}.default
@@ -25,7 +30,7 @@
             pkgs.pnpm
           ];
           shellHook = ''
-            export PLAYWRIGHT_BROWSERS_PATH=${pkgs.playwright.browsers}
+            export PLAYWRIGHT_BROWSERS_PATH=${unstablePkgs.playwright.browsers}
             source <(COMPLETE=''${SHELL##*/} mk)
           '';
         };
