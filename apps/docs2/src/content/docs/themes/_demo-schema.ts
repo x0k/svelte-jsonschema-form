a858d86864e5bd0b66deea0ba581ff07ba7fceb5
Roman Krasilnikov <x0kill@ya.ru>
2025-09-22 03:24:36 +0300

    [docs] Add `Supported validation events` block on all theme pages

--- apps/docs2/src/content/docs/themes/_demo-schema.ts
+++ apps/docs2/src/content/docs/themes/_demo-schema.ts
@@ -1,3 +1,9 @@
+import type { UiSchema } from "@sjsf/form";
+import { transpose } from "@json-table/core/lib/matrix";
+import { createMatrix, fromMatrix } from "@json-table/core/block-matrix";
+import { makeBlockFactory } from "@json-table/core/json-to-table";
+import { blockToHTML } from "@json-table/core/block-to-html";
+
 import { s } from "testing/demo";
 
 import { THEME_PACKAGES, type Theme } from "@/shared";
@@ -23,7 +29,56 @@ export function createExtraImports(theme: Theme, widgets: string[]) {
 }
 
 export function createSchemas(specs: s.Specs = {}) {
-  return s.createSchemas(specs);
+  const clone: s.Specs = {};
+  for (const [k, v] of Object.entries(specs)) {
+    clone[k] = v.with(1, {
+      ...v[1],
+      "ui:options": {
+        ...v[1]["ui:options"],
+        title: k,
+      },
+    }) as typeof v;
+  }
+  const schemas = s.createSchemas(clone);
+  const ui = schemas.uiSchema as UiSchema;
+  for (const key of Object.keys(ui)) {
+    ui[key] = {
+      ...ui[key],
+      "ui:options": {
+        ...(ui[key] as UiSchema)?.["ui:options"],
+        layouts: {
+          "object-properties": {
+            style:
+              "display: grid; grid-template-columns: repeat(2, 1fr); gap: 1rem;",
+          },
+        },
+      },
+    };
+  }
+  return schemas;
+}
+
+const createBlock = makeBlockFactory({
+  cornerCellValue: "№",
+  joinPrimitiveArrayValues: true,
+});
+
+export function validationEvents(specs: s.Specs) {
+  const data = Object.fromEntries(
+    Object.entries(specs).map(([k, [, , e]]) => [
+      k,
+      Object.keys(e).join(", ") ?? "none",
+    ])
+  );
+  let block = createBlock(data);
+  let matrix = createMatrix(block, ({ type, value }) => ({ type, value }));
+  matrix = transpose(matrix);
+  block = fromMatrix(
+    matrix,
+    ({ type }) => type,
+    ({ value }) => value
+  );
+  return blockToHTML(block);
 }
 
 export function replacer(_: string, value: any) {
