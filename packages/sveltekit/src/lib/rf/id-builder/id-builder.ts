d78d26ca95ab668aef77593f9e67aa541279dca1
Roman Krasilnikov <x0kill@ya.ru>
2025-10-04 19:19:30 +0300

    [sveltekit] Add SvelteKit ID Builder

--- packages/sveltekit/src/lib/rf/id-builder/id-builder.ts
+++ packages/sveltekit/src/lib/rf/id-builder/id-builder.ts
@@ -1,31 +1,59 @@
-import { decodePseudoElement, type FormIdBuilder, type Schema } from '@sjsf/form';
+import { isSchemaObject } from '@sjsf/form/lib/json-schema';
+import { getSchemaDefinitionByPath, isMultiSelect, type SchemaDefinition } from '@sjsf/form/core';
+import {
+  decodePseudoElement,
+  type FieldPseudoElement,
+  type FormIdBuilder,
+  type FormMerger,
+  type Schema,
+  type Validator
+} from '@sjsf/form';
+
+import { encode } from './codec.js';
 
 export interface FormIdBuilderOptions {
   schema: Schema;
   idPrefix: string;
-  pseudoSeparator?: string
+  pseudoSeparator?: string;
+  validator: () => Validator;
+  merger: () => FormMerger;
 }
 
-export const DEFAULT_PSEUDO_SEPARATOR = "$$"
+export const DEFAULT_PSEUDO_SEPARATOR = '::';
 
 export function createFormIdBuilder({
   idPrefix,
-  schema,
-  pseudoSeparator = DEFAULT_PSEUDO_SEPARATOR
+  schema: rootSchema,
+  pseudoSeparator = DEFAULT_PSEUDO_SEPARATOR,
+  validator,
+  merger
 }: FormIdBuilderOptions): FormIdBuilder {
   return {
     fromPath: (path) => {
       let str = '';
-      const currentSchema = schema
+      let currentSchema: SchemaDefinition | undefined = rootSchema;
+      let pseudo: FieldPseudoElement | undefined;
+      let isPseudoUndefined = true;
       for (let i = 0; i < path.length; i++) {
         const p = path[i]!;
-        const pseudo = decodePseudoElement(p);
-        str +=
-          pseudo !== undefined
-            ? `${pseudoSeparator}${pseudo}`
-            : typeof p === 'string' ? '.' + p : `[${p}]`;
+        pseudo = decodePseudoElement(p);
+        isPseudoUndefined = pseudo === undefined;
+        if (isPseudoUndefined) {
+          currentSchema = getSchemaDefinitionByPath(rootSchema, currentSchema, [p]);
+          str += typeof p === 'string' ? '.' + encode(p) : `[${p}]`;
+        } else {
+          str += encode(`${pseudoSeparator}${pseudo}`);
+        }
       }
-      return `${idPrefix}${str}`;
+      if (
+        isPseudoUndefined &&
+        currentSchema &&
+        isSchemaObject(currentSchema) &&
+        isMultiSelect(validator(), merger(), currentSchema, rootSchema)
+      ) {
+        str += '[]';
+      }
+      return `${encode(idPrefix)}${str}`;
     }
   };
 }
