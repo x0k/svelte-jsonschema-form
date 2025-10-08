8e427d93723b2a1b4b1dacc3b3bd4cab4857db0c
Roman Krasilnikov <x0kill@ya.ru>
2025-10-08 03:22:31 +0300

    [sveltekit] WIP: Add simple schema test case

--- packages/sveltekit/src/lib/rf/server/schema-value-parser.test.ts
+++ packages/sveltekit/src/lib/rf/server/schema-value-parser.test.ts
@@ -1,13 +1,18 @@
+import { describe, beforeEach, expect, it } from 'vitest';
+import type { Schema } from '@sjsf/form';
 import { createMerger } from '@sjsf/form/mergers/modern';
 import { createFormValidator } from '@sjsf/ajv8-validator';
 
-import { parseSchemaValue, type SchemaValueParserOptions } from './schema-value-parser.js';
-import { createFormDataEntryConverter } from '../../server/convert-form-data-entry.js';
-import { describe } from 'node:test';
-import { beforeEach, expect, it } from 'vitest';
+import { createFormDataEntryConverter } from '$lib/server/convert-form-data-entry.js';
+
+import {
+  parseSchemaValue,
+  type Input,
+  type SchemaValueParserOptions
+} from './schema-value-parser.js';
 
 const opts = ({
-  input = {},
+  input,
   schema = {},
   uiSchema = {},
   validator = createFormValidator(),
@@ -38,5 +43,83 @@ describe('parseSchemaValue', async () => {
 
   it('Should parse empty form', async () => {
     await expect(parseSchemaValue(c.signal, opts())).resolves.toBeUndefined();
+  });
+
+  it('Should revive empty array', async () => {
+    const schema: Schema = {
+      title: 'Locations Checkboxes',
+      type: 'array',
+      uniqueItems: true,
+      items: {
+        enum: ['foo', 'bar', 'baz']
+      }
+    };
+    await expect(parseSchemaValue(c.signal, opts({ schema }))).resolves.toEqual([]);
+  });
+
+  it('Should parse root value', async () => {
+    await expect(
+      parseSchemaValue(
+        c.signal,
+        opts({
+          schema: { type: 'string' },
+          input: 'value'
+        })
+      )
+    ).resolves.toBe('value');
+  });
+
+  it('Should parse simple schema', async () => {
+    const schema: Schema = {
+      title: 'A registration form',
+      description: 'A simple form example.',
+      type: 'object',
+      required: ['firstName', 'lastName'],
+      properties: {
+        firstName: {
+          type: 'string',
+          title: 'First name',
+          default: 'Chuck'
+        },
+        lastName: {
+          type: 'string',
+          title: 'Last name'
+        },
+        age: {
+          type: 'integer',
+          title: 'Age'
+        },
+        bio: {
+          type: 'string',
+          title: 'Bio'
+        },
+        password: {
+          type: 'string',
+          title: 'Password',
+          minLength: 3
+        },
+        telephone: {
+          type: 'string',
+          title: 'Telephone',
+          minLength: 10
+        }
+      }
+    };
+    const input: Input<FormDataEntryValue> = {
+      firstName: 'Chuck',
+      lastName: 'Norris',
+      age: '75',
+      bio: 'Roundhouse kicking asses since 1940',
+      password: 'noneed',
+      telephone: '1-800-KICKASS'
+    };
+    await expect(parseSchemaValue(c.signal, opts({ schema, input }))).resolves.toEqual({
+      firstName: 'Chuck',
+      lastName: 'Norris',
+      age: 75,
+      bio: 'Roundhouse kicking asses since 1940',
+      password: 'noneed',
+      telephone: '1-800-KICKASS'
+    });
   });
 });
--- packages/sveltekit/src/lib/rf/server/schema-value-parser.ts
+++ packages/sveltekit/src/lib/rf/server/schema-value-parser.ts
@@ -2,11 +2,16 @@ import { isSchemaObject } from '@sjsf/form/lib/json-schema';
 import {
   getClosestMatchingOption,
   getDiscriminatorFieldFromSchema,
+  getSimpleSchemaType,
+  isSchemaArrayValue,
+  isSchemaObjectValue,
   isSelect,
   resolveRef,
   type Merger,
   type Path,
+  type SchemaArrayValue,
   type SchemaDefinition,
+  type SchemaObjectValue,
   type SchemaValue
 } from '@sjsf/form/core';
 import {
@@ -20,8 +25,9 @@ import {
 } from '@sjsf/form';
 
 import { ONE_OF, type EntryConverter } from '$lib/model.js';
+import { isRecord } from '@sjsf/form/lib/object';
 
-type Input<T> = T | { [key: string]: Input<T> } | Input<T>[];
+export type Input<T> = T | { [key: string]: Input<T> } | Input<T>[] | undefined;
 
 export interface SchemaValueParserOptions<T> {
   schema: Schema;
@@ -29,7 +35,7 @@ export interface SchemaValueParserOptions<T> {
   input: Input<T>;
   validator: Validator;
   merger: Merger;
-  convertEntry: EntryConverter<T>;
+  convertEntry: EntryConverter<T | undefined>;
 }
 
 export function parseSchemaValue<T>(
@@ -87,6 +93,58 @@ export function parseSchemaValue<T>(
     );
   }
 
+  async function parseArray(
+    schema: Schema,
+    uiSchema: UiSchema,
+    input: Input<T>,
+    value: SchemaArrayValue | undefined
+  ): Promise<SchemaValue | undefined> {
+    if (!Array.isArray(input)) {
+      return value;
+    }
+  }
+
+  async function parseObject(
+    schema: Schema,
+    uiSchema: UiSchema,
+    input: Input<T>,
+    value: SchemaObjectValue
+  ) {
+    if (!isRecord(input)) {
+      return value;
+    }
+
+    async function setProperty(
+      property: string,
+      schemaDef: SchemaDefinition,
+      uiSchema: UiSchemaDefinition
+    ) {
+      if (value[property] === undefined) {
+        const propValue = await parseSchemaDef(
+          schemaDef,
+          uiSchema,
+          (input as Record<string, Input<T>>)[property],
+          undefined
+        );
+        if (propValue !== undefined) {
+          value[property] = propValue;
+        }
+      }
+    }
+
+    const visited = new Set<string>();
+    const { properties, additionalProperties } = schema;
+    if (properties !== undefined) {
+      const keys = Object.keys(properties);
+      for (let i = 0; i < keys.length; i++) {
+        const key = keys[i];
+        await setProperty(key, properties[key], (uiSchema[key] ?? {}) as UiSchema);
+        visited.add(key);
+      }
+    }
+    return value;
+  }
+
   async function parseSchemaDef(
     schema: SchemaDefinition,
     uiSchema: UiSchemaDefinition,
@@ -103,6 +161,14 @@ export function parseSchemaValue<T>(
     }
     if (oneOf) {
       value = await parseCombination(ONE_OF, oneOf, uiSchema.oneOf ?? uiSchema, schema, value);
+    }
+    const type = getSimpleSchemaType(schema);
+    if (type === 'object') {
+      value = await parseObject(schema, uiSchema, input, isSchemaObjectValue(value) ? value : {});
+    } else if (type === 'array') {
+      value = await parseArray(schema, uiSchema, input, isSchemaArrayValue(value) ? value : []);
+    } else if (value === undefined) {
+      value = await convert(schema, uiSchema, input);
     }
     return value;
   }
