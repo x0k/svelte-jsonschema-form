c1f1a12807c34a1b1ff425af9e67df2267164d23
Roman Krasilnikov <x0kill@ya.ru>
2025-10-17 22:30:51 +0300

    [form] Fix missing types export

--- packages/form/src/theme/index.ts
+++ packages/form/src/theme/index.ts
@@ -3,6 +3,8 @@ import { fromRecord } from "@/lib/resolver.js";
 import "../fields/exports.js";
 import "../templates/exports.js";
 
-import { definitions } from "./definitions.js";
+import { definitions, type ExtraComponents } from "./definitions.js";
+
+export type { ExtraComponents }
 
 export const base = fromRecord(definitions);
