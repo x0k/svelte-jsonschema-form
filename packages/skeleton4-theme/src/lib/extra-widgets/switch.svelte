ff415a1d016a8fb4b13f1551d0028237c0d89320
Roman Krasilnikov <x0kill@ya.ru>
2025-10-27 12:00:26 +0300

    [skeleton4] Fix switch widget

--- packages/skeleton4-theme/src/lib/extra-widgets/switch.svelte
+++ packages/skeleton4-theme/src/lib/extra-widgets/switch.svelte
@@ -1,22 +1,18 @@
 <script lang="ts" module>
 	import type { ComponentProps as SvelteComponentProps } from 'svelte';
-	import { Switch as SkeletonSwitch } from '@skeletonlabs/skeleton-svelte';
+	import type { SwitchRootProps } from '@skeletonlabs/skeleton-svelte';
 	import '@sjsf/form/fields/extra-widgets/switch';
 
 	declare module '@sjsf/form' {
 		interface UiOptions {
-			skeleton4Switch?: SvelteComponentProps<typeof SkeletonSwitch>;
+			skeleton4Switch?: SwitchRootProps;
 		}
 	}
 </script>
 
 <script lang="ts">
-	import {
-		customInputAttributes,
-		getFormContext,
-		createId,
-		type ComponentProps
-	} from '@sjsf/form';
+	import { customInputAttributes, getFormContext, createId, type ComponentProps } from '@sjsf/form';
+	import { Switch } from '@skeletonlabs/skeleton-svelte';
 
 	let { config, value = $bindable(), handlers, errors }: ComponentProps['switchWidget'] = $props();
 
@@ -25,7 +21,7 @@
 	const id = $derived(createId(ctx, config.path));
 </script>
 
-<SkeletonSwitch
+<Switch
 	{...customInputAttributes(ctx, config, 'skeleton4Switch', {
 		ids: {
 			hiddenInput: id
@@ -42,5 +38,9 @@
 		checked: value
 	})}
 >
-	{config.title}
-</SkeletonSwitch>
+	<Switch.Control>
+		<Switch.Thumb />
+	</Switch.Control>
+	<Switch.Label>{config.title}</Switch.Label>
+	<Switch.HiddenInput />
+</Switch>
