e7985907297d301abd9ece7e1d6cd12177ade74c
Roman Krasilnikov <x0kill@ya.ru>
2025-10-28 15:24:50 +0300

    [themes] Remove redundant templates import

--- packages/shadcn4-theme/src/lib/theme/definitions.ts
+++ packages/shadcn4-theme/src/lib/theme/definitions.ts
@@ -1,5 +1,4 @@
 import type { ComponentDefinitions } from '@sjsf/form';
-import * as templates from '@sjsf/form/templates/exports';
 
 import * as components from './components/exports.js';
 import * as widgets from './widgets/exports.js';
@@ -7,10 +6,8 @@ import * as widgets from './widgets/exports.js';
 export interface ExtraWidgets {}
 
 export const definitions = {
-	...templates,
 	...components,
 	...widgets
-} satisfies Partial<ComponentDefinitions> as typeof templates &
-	typeof components &
+} satisfies Partial<ComponentDefinitions> as typeof components &
 	typeof widgets &
 	Pick<ComponentDefinitions, keyof ExtraWidgets>;
