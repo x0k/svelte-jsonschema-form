2b1df03f60f58c5cb2333a1b1065a5f4dd8f0326
Roman Krasilnikov <x0kill@ya.ru>
2025-10-30 04:34:28 +0300

    [examples] Add shadcn extras theme starter

---
+++ examples/shadcn-extras-starter/src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import { setThemeContext } from "@sjsf/shadcn4-theme";
+  import * as components from "@sjsf/shadcn4-theme/new-york";
+  import * as extraComponents from '@sjsf-lab/shadcn-extras-theme/ui';
+
+  import "../app.css";
+
+  const { children } = $props();
+
+  setThemeContext({ components: { ...components, ...extraComponents } });
+</script>
+
+{@render children()}
