236f9c6d4b34bd3c64e58fc1347f2698d33a224f
Roman Krasilnikov <x0kill@ya.ru>
2025-11-03 01:10:11 +0300

    [basic] Remove select padding

--- packages/basic-theme/src/widgets/select.svelte
+++ packages/basic-theme/src/widgets/select.svelte
@@ -27,7 +27,7 @@
 
   const { placeholder, ...attributes } = $derived(
     selectAttributes(ctx, config, "select", handlers, {
-      style: "flex-grow: 1; padding: 1px 2px;",
+      style: "flex-grow: 1;",
     })
   );
 
