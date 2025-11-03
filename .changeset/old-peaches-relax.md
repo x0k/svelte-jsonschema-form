4cc459b2cb0f93114a673618f9969c0651bc907b
Roman Krasilnikov <x0kill@ya.ru>
2025-11-03 15:29:43 +0300

    [form] Simplify modern ID builder

---
+++ .changeset/old-peaches-relax.md
@@ -0,0 +1,5 @@
+---
+"@sjsf/form": major
+---
+
+Simplify modern ID builder
--- packages/form/src/id-builders/modern.ts
+++ packages/form/src/id-builders/modern.ts
@@ -7,20 +7,14 @@ import {
 
 export interface IdOptions {
   idPrefix?: string;
-  propertySeparator?: string;
-  indexSeparator?: string;
-  pseudoSeparator?: string;
+  separator?: string;
 }
 
-export const DEFAULT_PROPERTY_SEPARATOR = ".";
-export const DEFAULT_INDEX_SEPARATOR = "@";
-export const DEFAULT_PSEUDO_SEPARATOR = "::";
+export const DEFAULT_SEPARATOR = "_";
 
 export function createFormIdBuilder({
   idPrefix = DEFAULT_ID_PREFIX,
-  indexSeparator = DEFAULT_INDEX_SEPARATOR,
-  propertySeparator = DEFAULT_PROPERTY_SEPARATOR,
-  pseudoSeparator = DEFAULT_PSEUDO_SEPARATOR,
+  separator = DEFAULT_SEPARATOR,
 }: IdOptions = {}): FormIdBuilder {
   return {
     fromPath: (path: FieldPath) => {
@@ -30,8 +24,8 @@ export function createFormIdBuilder({
         const pseudo = decodePseudoElement(p);
         str +=
           pseudo !== undefined
-            ? `${pseudoSeparator}${pseudo}`
-            : `${typeof p === "string" ? propertySeparator : indexSeparator}${p}`;
+            ? `${separator}${separator}${pseudo}`
+            : `${separator}${p}`;
       }
       return `${idPrefix}${str}`;
     },
