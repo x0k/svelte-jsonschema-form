057e549f0912b69570d6a80079391331f26e96ca
Roman Krasilnikov <x0k@users.noreply.github.com>
2025-11-10 10:42:53 +0300

    Fix advanced examples (#243)
    * [docs] Use correct ID builders for SvelteKit examples
    
    * [build] Fix peer dependencies for `shadcn-svelte` and `schadcn-svelte-extras` theme examples

--- apps/docs2/src/web-ide/examples/form-actions-without-js.ts
+++ apps/docs2/src/web-ide/examples/form-actions-without-js.ts
@@ -2,10 +2,15 @@ import packageJson from "%/sveltekit-starter/package.json";
 import pageServerTs from "%/sveltekit-starter/src/routes/form-actions-without-js/+page.server.ts?raw";
 import pageSvelte from "%/sveltekit-starter/src/routes/form-actions-without-js/+page.svelte?raw";
 
+import { FORM_SVELTEKIT_PACKAGE } from '@/shared';
+
 import { omitBasePackages, type Layer } from "../layer";
 
 export const layer = {
   package: omitBasePackages(packageJson),
+  formDefaults: {
+    idBuilderPackage: FORM_SVELTEKIT_PACKAGE
+  },
   files: {
     "src/routes/+page.server.ts": pageServerTs,
     "src/routes/+page.svelte": pageSvelte,
--- apps/docs2/src/web-ide/examples/remote-functions-without-js.ts
+++ apps/docs2/src/web-ide/examples/remote-functions-without-js.ts
@@ -4,10 +4,15 @@ import dataRemoteTs from "%/sveltekit-starter/src/routes/remote-functions-withou
 import pageSvelte from "%/sveltekit-starter/src/routes/remote-functions-without-js/+page.svelte?raw";
 import formContentSvelte from "%/sveltekit-starter/src/routes/remote-functions-without-js/form-content.svelte?raw";
 
+import { FORM_SVELTEKIT_RF_PACKAGE } from '@/shared';
+
 import { omitBasePackages, type Layer } from "../layer";
 
 export const layer = {
   package: omitBasePackages(packageJson),
+  formDefaults: {
+    idBuilderPackage: FORM_SVELTEKIT_RF_PACKAGE
+  },
   files: {
     "src/lib/post.ts": postModelTs,
     "src/routes/data.remote.ts": dataRemoteTs,
