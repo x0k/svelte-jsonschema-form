d2758759cc7df6f6162eac4146f5e6d48e4fe0a7
Roman Krasilnikov <x0kill@ya.ru>
2025-11-24 13:00:26 +0300

    [examples] Fix styles in sveltekit examples

---
+++ examples/sveltekit-starter/src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import type { Snippet } from 'svelte';
+
+  import '@sjsf/basic-theme/css/basic.css';
+
+  const { children }: { children: Snippet } = $props()
+</script>
+
+{@render children()}
