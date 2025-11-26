ed2837687a6e0827ed0f481b671f55c706e9dded
x0k <x0k@users.noreply.github.com>
2025-11-26 11:36:51 +0000

    deploy: cf50fc9a82691fec7e44aefbf08fe77b81167808

---
+++ _astro/basic.Csh5k_F5.js
@@ -0,0 +1,10 @@
+import{p as t}from"./package.yOgv3FOl.js";import{o as r}from"./advanced-examples.DMsG3v_S.js";import"./each.BERe9yIu.js";import"./render.B5rVQQAv.js";import"./definitions.DM0KYeNY.js";import"./_commonjsHelpers.DaWZu8wl.js";import"./snippet.Bu_s7wcY.js";import"./shared.Dt5juWKd.js";import"./preload-helper.BUFao3bW.js";import"./buttons.BWwP8odj.js";/* empty css                                                       *//* empty css                                                                 */const o=`<script lang="ts">
+  import type { Snippet } from 'svelte';
+
+  import '@sjsf/basic-theme/css/basic.css';
+
+  const { children }: { children: Snippet } = $props()
+<\/script>
+
+{@render children()}
+`,g={package:r(t),formDefaults:{theme:"basic"},files:{"src/routes/+layout.svelte":o}};export{g as layer};
---
+++ _astro/legacy.B84qf8KZ.js
@@ -0,0 +1,1 @@
+import{Z as a}from"./render.B5rVQQAv.js";a();
