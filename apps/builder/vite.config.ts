20e9e1544a47fe0a53221f11b173ff91e58c8091
Roman Krasilnikov <x0kill@ya.ru>
2025-12-08 11:34:42 +0300

    [builder] Use icons for node factories

--- apps/builder/vite.config.ts
+++ apps/builder/vite.config.ts
@@ -1,20 +1,21 @@
-import { resolve } from "node:path";
-import { fileURLToPath } from "node:url";
-import { defineConfig } from "vite";
-import tailwindcss from "@tailwindcss/vite";
-import { svelte } from "@sveltejs/vite-plugin-svelte";
+import { resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+import Icons from 'unplugin-icons/vite';
 
 export default defineConfig({
-  base: "/svelte-jsonschema-form/builder3/",
-  plugins: [tailwindcss(), svelte()],
-  resolve: {
-    alias: {
-      $lib: resolve("./src/lib"),
-      $apps: fileURLToPath(new URL("..", import.meta.url)),
-    },
-  },
-  optimizeDeps: {
-    exclude: ["@jis3r/icons"],
-    include: ['@svar-ui/svelte-core', "svelte-sonner"]
-  },
+	base: '/svelte-jsonschema-form/builder3/',
+	plugins: [tailwindcss(), svelte(), Icons({ compiler: 'svelte' })],
+	resolve: {
+		alias: {
+			$lib: resolve('./src/lib'),
+			$apps: fileURLToPath(new URL('..', import.meta.url))
+		}
+	},
+	optimizeDeps: {
+		exclude: ['@jis3r/icons'],
+		include: ['@svar-ui/svelte-core', 'svelte-sonner']
+	}
 });
