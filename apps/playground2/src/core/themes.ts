a28e29ece015fc605d1598469de4bb847eba1ac7
Roman Krasilnikov <x0kill@ya.ru>
2025-12-09 21:00:37 +0300

    [playground] Add `beercss` theme

--- apps/playground2/src/core/themes.ts
+++ apps/playground2/src/core/themes.ts
@@ -10,9 +10,9 @@ import "@sjsf/form/fields/extra/array-native-files-include";
 import "@sjsf/form/fields/extra/array-tags-include";
 
 import { theme as basic } from "@sjsf/basic-theme";
-import basicStyles from '@sjsf/basic-theme/css/basic.css?raw'
-import picoStyles from '@picocss/pico/css/pico.css?raw'
-import picoAdapterStyles from '@sjsf/basic-theme/css/pico.css?raw'
+import basicStyles from "@sjsf/basic-theme/css/basic.css?raw";
+import picoStyles from "@picocss/pico/css/pico.css?raw";
+import picoAdapterStyles from "@sjsf/basic-theme/css/pico.css?raw";
 import "@sjsf/basic-theme/extra-widgets/checkboxes-include";
 import "@sjsf/basic-theme/extra-widgets/date-picker-include";
 import "@sjsf/basic-theme/extra-widgets/file-include";
@@ -72,6 +72,7 @@ import shadcn4Styles from "@sjsf/shadcn4-theme/styles.css?raw";
 import "@sjsf/shadcn4-theme/extra-widgets/checkboxes-include";
 import "@sjsf/shadcn4-theme/extra-widgets/combobox-include";
 import "@sjsf/shadcn4-theme/extra-widgets/date-picker-include";
+import "@sjsf/shadcn4-theme/extra-widgets/date-range-picker-include";
 import "@sjsf/shadcn4-theme/extra-widgets/file-include";
 import "@sjsf/shadcn4-theme/extra-widgets/multi-select-include";
 import "@sjsf/shadcn4-theme/extra-widgets/radio-buttons-include";
@@ -82,11 +83,103 @@ import "@sjsf/shadcn4-theme/extra-widgets/textarea-include";
 
 import { theme as svar } from "@sjsf-lab/svar-theme";
 import "@sjsf-lab/svar-theme/extra-widgets/checkboxes-include";
+import "@sjsf-lab/svar-theme/extra-widgets/color-picker-include";
+import "@sjsf-lab/svar-theme/extra-widgets/color-select-include";
+import "@sjsf-lab/svar-theme/extra-widgets/combobox-include";
 import "@sjsf-lab/svar-theme/extra-widgets/date-picker-include";
+import "@sjsf-lab/svar-theme/extra-widgets/date-range-picker-include";
 import "@sjsf-lab/svar-theme/extra-widgets/multi-select-include";
 import "@sjsf-lab/svar-theme/extra-widgets/radio-include";
+import "@sjsf-lab/svar-theme/extra-widgets/radio-buttons-include";
 import "@sjsf-lab/svar-theme/extra-widgets/range-include";
+import "@sjsf-lab/svar-theme/extra-widgets/switch-include";
 import "@sjsf-lab/svar-theme/extra-widgets/textarea-include";
+
+import { theme as beercss } from "@sjsf-lab/beercss-theme";
+import beercssStyles from 'beercss/dist/cdn/beer.css?raw'
+const beerCssSettings = `.light {
+  --primary: #6750a4;
+  --on-primary: #ffffff;
+  --primary-container: #e9ddff;
+  --on-primary-container: #22005d;
+  --secondary: #625b71;
+  --on-secondary: #ffffff;
+  --secondary-container: #e8def8;
+  --on-secondary-container: #1e192b;
+  --tertiary: #7e5260;
+  --on-tertiary: #ffffff;
+  --tertiary-container: #ffd9e3;
+  --on-tertiary-container: #31101d;
+  --error: #ba1a1a;
+  --on-error: #ffffff;
+  --error-container: #ffdad6;
+  --on-error-container: #410002;
+  --background: #fffbff;
+  --on-background: #1c1b1e;
+  --surface: #fdf8fd;
+  --on-surface: #1c1b1e;
+  --surface-variant: #e7e0eb;
+  --on-surface-variant: #49454e;
+  --outline: #7a757f;
+  --outline-variant: #cac4cf;
+  --shadow: #000000;
+  --scrim: #000000;
+  --inverse-surface: #313033;
+  --inverse-on-surface: #f4eff4;
+  --inverse-primary: #cfbcff;
+  --surface-dim: #ddd8dd;
+  --surface-bright: #fdf8fd;
+  --surface-container-lowest: #ffffff;
+  --surface-container-low: #f7f2f7;
+  --surface-container: #f2ecf1;
+  --surface-container-high: #ece7eb;
+  --surface-container-highest: #e6e1e6;
+}
+.dark {
+  --primary: #cfbcff;
+  --on-primary: #381e72;
+  --primary-container: #4f378a;
+  --on-primary-container: #e9ddff;
+  --secondary: #cbc2db;
+  --on-secondary: #332d41;
+  --secondary-container: #4a4458;
+  --on-secondary-container: #e8def8;
+  --tertiary: #efb8c8;
+  --on-tertiary: #4a2532;
+  --tertiary-container: #633b48;
+  --on-tertiary-container: #ffd9e3;
+  --error: #ffb4ab;
+  --on-error: #690005;
+  --error-container: #93000a;
+  --on-error-container: #ffb4ab;
+  --background: #1c1b1e;
+  --on-background: #e6e1e6;
+  --surface: #141316;
+  --on-surface: #e6e1e6;
+  --surface-variant: #49454e;
+  --on-surface-variant: #cac4cf;
+  --outline: #948f99;
+  --outline-variant: #49454e;
+  --shadow: #000000;
+  --scrim: #000000;
+  --inverse-surface: #e6e1e6;
+  --inverse-on-surface: #313033;
+  --inverse-primary: #6750a4;
+  --surface-dim: #141316;
+  --surface-bright: #3a383c;
+  --surface-container-lowest: #0f0e11;
+  --surface-container-low: #1c1b1e;
+  --surface-container: #201f22;
+  --surface-container-high: #2b292d;
+  --surface-container-highest: #363438;
+}`
+import "@sjsf-lab/beercss-theme/extra-widgets/checkboxes-include";
+import "@sjsf-lab/beercss-theme/extra-widgets/date-picker-include";
+import "@sjsf-lab/beercss-theme/extra-widgets/file-include";
+import "@sjsf-lab/beercss-theme/extra-widgets/radio-include";
+import "@sjsf-lab/beercss-theme/extra-widgets/range-include";
+import "@sjsf-lab/beercss-theme/extra-widgets/switch-include";
+import "@sjsf-lab/beercss-theme/extra-widgets/textarea-include";
 
 export const themes = {
   basic,
@@ -96,6 +189,7 @@ export const themes = {
   skeleton4,
   shadcn4,
   svar,
+  beercss,
 };
 
 export const themeStyles = {
@@ -106,4 +200,5 @@ export const themeStyles = {
   shadcn4: shadcn4Styles,
   svar: "",
   pico: `${picoStyles}${picoAdapterStyles}`,
+  beercss: `${beercssStyles}\n${beerCssSettings}`,
 } satisfies Record<keyof typeof themes, string>;
--- apps/playground2/svelte.config.js
+++ apps/playground2/svelte.config.js
@@ -1,6 +1,6 @@
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
-const injectedCss = ["@jis3r/icons", "svar"];
+const injectedCss = ["@jis3r/icons", "svar", "beercss"];
 
 export default {
   // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
