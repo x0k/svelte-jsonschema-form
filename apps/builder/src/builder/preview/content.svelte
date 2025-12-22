eade405bf33eb45d2beef4bc77231c2a017c8bfc
Roman Krasilnikov <x0kill@ya.ru>
2025-12-22 04:00:30 +0300

    [builder] Use file type icons

--- apps/builder/src/builder/preview/content.svelte
+++ apps/builder/src/builder/preview/content.svelte
@@ -1,68 +1,84 @@
 <script lang="ts">
-  import Code, { type CodeFile } from "$lib/components/code.svelte";
+	import { isRecordEmpty } from '@sjsf/form/lib/object';
 
-  import { getBuilderContext } from "../context.svelte.js";
-  import { PreviewSubRouteName, type PreviewRoute } from "../model.js";
-  import Form from "./form.svelte";
+	import Code, { type CodeFile } from '$lib/components/code.svelte';
 
-  const { route }: { route: PreviewRoute } = $props();
+	import DeviconPlainSvelte from '~icons/devicon-plain/svelte';
+	import DeviconPlainTypescript from '~icons/devicon-plain/typescript';
+	import DeviconPlainBash from '~icons/devicon-plain/bash';
+	import DeviconPlainCss from '~icons/devicon-plain/css';
+	import MdiCodeJson from '~icons/mdi/code-json';
 
-  const ctx = getBuilderContext();
+	import { getBuilderContext } from '../context.svelte.js';
+	import { PreviewSubRouteName, type PreviewRoute } from '../model.js';
+	import Form from './form.svelte';
 
-  const ROUTE_FILES: Record<PreviewSubRouteName, () => CodeFile[]> = {
-    [PreviewSubRouteName.Code]: () => {
-      const files: CodeFile[] = [
-        {
-          title: "form.svelte",
-          get content() {
-            return ctx.formDotSvelte;
-          },
-        },
-        {
-          title: "defaults.ts",
-          get content() {
-            return ctx.formDefaults;
-          },
-        },
-        {
-          title: "install.sh",
-          get content() {
-            return ctx.installSh;
-          },
-        },
-      ];
-      if (ctx.appCss.length > 0) {
-        files.push({
-          title: "app.css",
-          get content() {
-            return ctx.appCss;
-          },
-        });
-      }
-      return files;
-    },
-    [PreviewSubRouteName.Schema]: () => [
-      {
-        title: "schema.json",
-        get content() {
-          return ctx.highlight("json", JSON.stringify(ctx.schema, null, 2));
-        },
-      },
-      {
-        title: "ui-schema.json",
-        get content() {
-          return ctx.highlight(
-            "json",
-            JSON.stringify(ctx.uiSchema ?? {}, null, 2)
-          );
-        },
-      },
-    ],
-  };
+	const { route }: { route: PreviewRoute } = $props();
+
+	const ctx = getBuilderContext();
+
+	const ROUTE_FILES: Record<PreviewSubRouteName, () => CodeFile[]> = {
+		[PreviewSubRouteName.Code]: () => {
+			const files: CodeFile[] = [
+				{
+					Icon: DeviconPlainSvelte,
+					title: 'form.svelte',
+					get content() {
+						return ctx.formDotSvelte;
+					}
+				},
+				{
+					Icon: DeviconPlainTypescript,
+					title: 'defaults.ts',
+					get content() {
+						return ctx.formDefaults;
+					}
+				},
+				{
+					Icon: DeviconPlainBash,
+					title: 'install.sh',
+					get content() {
+						return ctx.installSh;
+					}
+				}
+			];
+			if (ctx.appCss.length > 0) {
+				files.splice(2, 0, {
+					Icon: DeviconPlainCss,
+					title: 'app.css',
+					get content() {
+						return ctx.appCss;
+					}
+				});
+			}
+			return files;
+		},
+		[PreviewSubRouteName.Schema]: () => {
+			const files = [
+				{
+					Icon: MdiCodeJson,
+					title: 'schema.json',
+					get content() {
+						return ctx.highlight('json', JSON.stringify(ctx.schema, null, 2));
+					}
+				}
+			];
+			if (ctx.uiSchema && !isRecordEmpty(ctx.uiSchema)) {
+				files.push({
+					Icon: MdiCodeJson,
+					title: 'ui-schema.json',
+					get content() {
+						return ctx.highlight('json', JSON.stringify(ctx.uiSchema ?? {}, null, 2));
+					}
+				});
+			}
+			return files;
+		}
+	};
 </script>
 
 {#if route.subRoute === undefined}
-  <Form />
+	<Form />
 {:else}
-  <Code files={ROUTE_FILES[route.subRoute]()} />
+	<Code files={ROUTE_FILES[route.subRoute]()} />
 {/if}
--- apps/builder/src/lib/components/code.svelte
+++ apps/builder/src/lib/components/code.svelte
@@ -1,61 +1,58 @@
 <script lang="ts" module>
-  export interface CodeFile {
-    title: string;
-    content: string;
-  }
+	export interface CodeFile {
+		Icon: Component<SVGAttributes<SVGSVGElement>>;
+		title: string;
+		content: string;
+	}
 </script>
 
 <script lang="ts">
-  import { untrack } from "svelte";
+	import { untrack, type Component } from 'svelte';
 
-  import { Button } from "$lib/components/ui/button/index.js";
-  import { CopyButton } from "$lib/components/copy-button/index.js";
+	import { Button } from '$lib/components/ui/button/index.js';
+	import { CopyButton } from '$lib/components/copy-button/index.js';
+	import type { SVGAttributes } from 'svelte/elements';
 
-  interface Props {
-    files: CodeFile[];
-  }
+	interface Props {
+		files: CodeFile[];
+	}
 
-  const { files }: Props = $props();
+	const { files }: Props = $props();
 
-  let selectedIndex = $derived(0);
-  $effect(() => {
-    files.length;
-    untrack(() => {
-      if (selectedIndex >= files.length) {
-        selectedIndex = files.length - 1;
-      }
-    });
-  });
-  let selected = $derived(files[selectedIndex]);
+	let selectedIndex = $derived(0);
+	$effect(() => {
+		files.length;
+		untrack(() => {
+			if (selectedIndex >= files.length) {
+				selectedIndex = files.length - 1;
+			}
+		});
+	});
+	let selected = $derived(files[selectedIndex]);
 </script>
 
 <div class="rounded-md border">
-  {#if selected}
-    <div class="flex gap-2 p-2 border-b">
-      {#each files as file, i (file.title)}
-        <Button
-          size="sm"
-          variant="ghost"
-          class={[
-            selectedIndex === i &&
-              "bg-accent text-accent-foreground dark:bg-accent/50",
-          ]}
-          onclick={() => {
-            selectedIndex = i;
-          }}>{file.title}</Button
-        >
-      {/each}
-      <CopyButton
-        class="ml-auto"
-        size="sm"
-        variant="ghost"
-        text={() => selected.content}
-      />
-    </div>
-    <div>
-      {@html selected.content}
-    </div>
-  {:else}
-    <p class="p-4 text-center text-muted-foreground">No files</p>
-  {/if}
+	{#if selected}
+		<div class="flex gap-2 border-b p-2">
+			{#each files as file, i (file.title)}
+				<Button
+					size="sm"
+					variant="ghost"
+					class={[selectedIndex === i && 'bg-accent text-accent-foreground dark:bg-accent/50']}
+					onclick={() => {
+						selectedIndex = i;
+					}}
+				>
+					<file.Icon />
+					{file.title}</Button
+				>
+			{/each}
+			<CopyButton class="ml-auto" size="sm" variant="ghost" text={() => selected.content} />
+		</div>
+		<div>
+			{@html selected.content}
+		</div>
+	{:else}
+		<p class="p-4 text-center text-muted-foreground">No files</p>
+	{/if}
 </div>
