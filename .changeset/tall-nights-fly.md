d08244729655b8891e9a025b4dd83004064592e2
Roman Krasilnikov <x0k@users.noreply.github.com>
2026-01-13 15:11:10 +0300

    [beercss] Fix UI options for `date-picker` and `switch` widgets (#307)

---
+++ .changeset/tall-nights-fly.md
@@ -0,0 +1,10 @@
+---
+"@sjsf-lab/beercss-theme": patch
+---
+
+[Breaking change] Fix UI options for `date-picker` and `switch` widgets
+
+Widgets now use the correct names for UI options:
+
+- `daisyui5Switch` -> `beercssSwitch`
+- `daisyui5PikadayCalendar` -> `datePicker`
--- lab/beercss-theme/src/lib/extra-widgets/date-picker.svelte
+++ lab/beercss-theme/src/lib/extra-widgets/date-picker.svelte
@@ -1,27 +1,10 @@
-<script lang="ts" module>
-	import type { HTMLInputAttributes } from 'svelte/elements';
-	import '@sjsf/form/fields/extra-widgets/date-picker';
-
-	declare module '@sjsf/form' {
-		interface UiOptions {
-			daisyui5PikadayCalendar?: HTMLInputAttributes;
-		}
-	}
-</script>
-
 <script lang="ts">
 	import { getFormContext, inputAttributes, type ComponentProps } from '@sjsf/form';
+	import '@sjsf/basic-theme/extra-widgets/date-picker.svelte';
 
-	let {
-		value = $bindable(),
-		handlers,
-		config
-	}: ComponentProps['datePickerWidget'] = $props();
+	let { value = $bindable(), handlers, config }: ComponentProps['datePickerWidget'] = $props();
 
 	const ctx = getFormContext();
 </script>
 
-<input
-	{...inputAttributes(ctx, config, 'daisyui5PikadayCalendar', handlers, { type: 'date' })}
-	bind:value
-/>
+<input {...inputAttributes(ctx, config, 'datePicker', handlers, { type: 'date' })} bind:value />
