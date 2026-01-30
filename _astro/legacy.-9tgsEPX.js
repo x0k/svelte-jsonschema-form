53584c9bd9a68fe2b6e96a60bbeda46ee8591422
x0k <x0k@users.noreply.github.com>
2026-01-30 22:22:48 +0000

    deploy: 0fddf78c89d09ac78620607bf5bfdbd23bdd03e0

--- _astro/legacy.-9tgsEPX.js
+++
@@ -1,1 +0,0 @@
-import{a8 as a}from"./render.-l4jZV6X.js";a();
---
+++ _astro/pico.ChA3NS_B.js
@@ -0,0 +1,17 @@
+import{p as s}from"./package.D2uID5XT.js";import{m as o,o as p}from"./advanced-examples.d7jkeSZ_.js";import"./each.0Sggf4I3.js";import"./render.DWbcC03b.js";import"./definitions.BpXZrVlp.js";import"./_commonjsHelpers.DaWZu8wl.js";import"./snippet.aAPlOa3D.js";import"./shared.BopwCmxQ.js";import"./preload-helper.BUFao3bW.js";import"./buttons.CHpFzLRV.js";/* empty css                                                       *//* empty css                                                                 */const h={package:o(p(s),{dependencies:{"@picocss/pico":"^2.1.0"}}),formDefaults:{theme:"basic"},files:{"src/routes/+layout.svelte":`<script lang="ts">
+  import type { Snippet } from 'svelte';
+
+  import '@picocss/pico/css/pico.css';
+  import '@sjsf/basic-theme/css/pico.css';
+
+  const { children }: { children: Snippet } = $props()
+<\/script>
+
+{@render children()}
+
+<style>
+  :global(html) {
+    padding: 2rem;
+  }
+</style>
+`}};export{h as layer};
