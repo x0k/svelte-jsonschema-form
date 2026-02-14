f8bdf1dc7d01247d64021c60e1d58be1f3d9e82a
x0k <x0k@users.noreply.github.com>
2026-02-14 19:26:16 +0000

    deploy: 12ed71ef44450307bef69f30aff2cd92407a6a71

---
+++ _astro/legacy.BZnN8vZF.js
@@ -0,0 +1,1 @@
+import{a9 as a}from"./render.BLPiGEwN.js";a();
---
+++ _astro/markdown-description.C_zAWyDl.js
@@ -0,0 +1,49 @@
+import{p as n}from"./package.D1u2wnRv.js";import{p as t,M as o}from"./advanced-examples.DISFAP0_.js";import"./each.tP9HQ3PH.js";import"./render.BLPiGEwN.js";import"./definitions.xTkvThOi.js";import"./_commonjsHelpers.DaWZu8wl.js";import"./snippet.DY9HY_LV.js";import"./shared.IKWVmtkT.js";import"./preload-helper.BUFao3bW.js";import"./buttons.BHgkw6S7.js";/* empty css                                                       *//* empty css                                                                 */const r=`<script lang="ts">
+  import Markdown from "svelte-exmarkdown";
+
+  import {
+    descriptionAttributes,
+    getFormContext,
+    type ComponentProps,
+  } from "@sjsf/form";
+
+  const { config, description }: ComponentProps["description"] = $props();
+
+  const ctx = getFormContext();
+<\/script>
+
+<Markdown md={description}>
+  {#snippet p({ children })}
+    <div {...descriptionAttributes(ctx, config, "descriptionAttributes", {})}>
+      {@render children?.()}
+    </div>
+  {/snippet}
+</Markdown>
+`,e=`<script lang="ts">
+  import { overrideByRecord } from '@sjsf/form/lib/resolver';
+  import { createForm, BasicForm, type Schema } from "@sjsf/form";
+
+  import * as defaults from "$lib/form-defaults";
+
+  import Description from './description.svelte';
+
+  const schema = {
+    type: "null",
+    title: "Title",
+    description: "**bold** *italic* [link](#)",
+  } as const satisfies Schema;
+
+  const theme = overrideByRecord(defaults.theme, {
+    description: Description,
+  });
+
+  const form = createForm({
+    ...defaults,
+    theme,
+    schema,
+    onSubmit: console.log,
+  });
+<\/script>
+
+<BasicForm {form} />
+`,h={package:t(n,o),files:{"src/routes/description.svelte":r,"src/routes/+page.svelte":e}};export{h as layer};
