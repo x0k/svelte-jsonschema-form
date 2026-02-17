16358ee9577bf67eee484745df6b139bdbdd78a0
Roman Krasilnikov <x0kill@ya.ru>
2026-02-17 09:10:12 +0300

    [form] Add `query` comment for `getStableConfig` method

--- packages/form/src/form/state/config.ts
+++ packages/form/src/form/state/config.ts
@@ -4,6 +4,9 @@ import { FORM_CONFIGS_CACHE } from "../internals.js";
 import type { Config } from "../config.js";
 import type { FormState } from "./state.js";
 
+/**
+ * @query
+ */
 export function getStableConfig<T>(ctx: FormState<T>, config: Config): Config {
   const cache = ctx[FORM_CONFIGS_CACHE];
   const cached = cache.get(config.path);
