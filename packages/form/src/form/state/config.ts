e8c5d510b8311aa131155ff80df07c1b9cb91510
Roman Krasilnikov <x0kill@ya.ru>
2026-02-18 08:10:30 +0300

    [form] Add TODO comment

--- packages/form/src/form/state/config.ts
+++ packages/form/src/form/state/config.ts
@@ -10,6 +10,7 @@ import type { FormState } from "./state.js";
 export function getStableConfig<T>(ctx: FormState<T>, config: Config): Config {
   const cache = ctx[FORM_CONFIGS_CACHE];
   const cached = cache.get(config.path);
+  // TODO: Create a separate function for comparing configs
   if (
     isOrderedSchemaDeepEqual(
       cached as unknown as Schema,
@@ -18,7 +19,9 @@ export function getStableConfig<T>(ctx: FormState<T>, config: Config): Config {
   ) {
     return cached!;
   }
-  // TODO: Snapshot?
+  // NOTE: A snapshot is not needed here because:
+  // 1. The function is usually called with a non-proxied value (object literal)
+  // 2. A stable reference to FieldPath will be lost
   cache.set(config.path, config);
   return config;
 }
