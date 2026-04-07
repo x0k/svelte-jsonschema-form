4966d9726b38aba89e45aa401f42a96b6af440fa
Roman Krasilnikov <x0kill@ya.ru>
2026-04-07 21:36:01 +0300

    [playground] WIP: Add support for 2020-12 JSON Schema validators

--- apps/playground2/src/core/validators.ts
+++ apps/playground2/src/core/validators.ts
@@ -1,18 +1,56 @@
-import { createFormValidator as ajv8 } from "@sjsf/ajv8-validator";
+import { ROOT_SCHEMA_PREFIX } from "@sjsf/form/core";
+import {
+  addFormComponents,
+  createFormValidator as ajv8,
+} from "@sjsf/ajv8-validator";
 import { createFormValidator as cfworker } from "@sjsf/cfworker-validator";
-import { createFormValidator as schemasafe } from "@sjsf/schemasafe-validator";
+import {
+  DEFAULT_VALIDATOR_OPTIONS,
+  createFormValidator as schemasafe,
+} from "@sjsf/schemasafe-validator";
 import Ajv2020 from "ajv/dist/2020.js";
+import addFormats from "ajv-formats";
+import {
+  Validator as CfValidator,
+  type Schema as CfSchema,
+} from "@cfworker/json-schema";
+import {
+  validator as safeValidator,
+  type Schema as SafeSchema,
+} from "@exodus/schemasafe";
 
 export const validators = {
-  ajv8,
+  ajv8: <T>(options: Parameters<typeof ajv8>[0]) =>
+    ajv8<T>({
+      ...options,
+      ajvPlugins: (ajv) => addFormComponents(addFormats(ajv)),
+    }),
   ajv8_2020: <T>(options: Parameters<typeof ajv8>[0]) =>
     ajv8<T>({
       ...options,
-      // Do not use `ts-expect-error` to avoid unused expect from docs build
-      Ajv: Ajv2020 as any,
+      Ajv: Ajv2020,
+      ajvPlugins: (ajv) => addFormComponents(addFormats(ajv)),
     }),
   cfworker,
+  cfworker_2020: <T>(options: Parameters<typeof cfworker>[0]) =>
+    cfworker<T>({
+      ...options,
+      factory: (schema) =>
+        new CfValidator(schema as CfSchema, "2020-12", false),
+    }),
   schemasafe,
+  schemasafe_2020: <T>(options: Parameters<typeof schemasafe>[0]) =>
+    schemasafe<T>({
+      ...options,
+      factory: (schema, rootSchema) =>
+        safeValidator(schema as SafeSchema, {
+          ...DEFAULT_VALIDATOR_OPTIONS,
+          $schemaDefault: "https://json-schema.org/draft/2020-12/schema",
+          schemas: {
+            [ROOT_SCHEMA_PREFIX]: rootSchema as SafeSchema,
+          },
+        }),
+    }),
   zod4: ajv8,
   valibot: ajv8,
 };
@@ -25,9 +63,11 @@ export const REAL_VALIDATORS = (Object.keys(validators) as Validator[]).filter(
 
 export const VALIDATOR_TITLES: Record<Validator, string> = {
   ajv8: "Ajv",
-  ajv8_2020: "Ajv (2020)",
-  cfworker: "@exodus/schemasafe",
-  schemasafe: "@cfworker/json-schema",
+  ajv8_2020: "Ajv (2020-12)",
+  cfworker: "@cfworker/json-schema",
+  cfworker_2020: "@cfworker/json-schema (2020-12)",
+  schemasafe: "@exodus/schemasafe",
+  schemasafe_2020: "@exodus/schemasafe (2020-12)",
   zod4: "Ajv",
   valibot: "Ajv",
 };
