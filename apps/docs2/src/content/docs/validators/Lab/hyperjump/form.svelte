7d20581e2f457110a88a2d8948e4b4294317fb80
Roman Krasilnikov <x0kill@ya.ru>
2026-04-17 02:06:08 +0300

    [docs] Display hyperjump validator form

---
+++ apps/docs2/src/content/docs/validators/Lab/hyperjump/form.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import { BasicForm, createForm, getValueSnapshot } from "@sjsf/form";
+  import { createFormValidatorFactory } from "@sjsf-lab/hyperjump-validator";
+  import { resolver } from "@sjsf/form/resolvers/compat";
+  import "@hyperjump/json-schema/draft-07";
+
+  import * as defaults from "@/lib/form/defaults";
+
+  import { schema, fieldsValidationMode } from "./patched-schema";
+  import { ast } from "./ast";
+
+  const form = createForm({
+    ...defaults,
+    schema,
+    validator: createFormValidatorFactory({ ast }),
+    fieldsValidationMode,
+    resolver,
+  });
+</script>
+
+<BasicForm {form} novalidate />
+
+<pre>{JSON.stringify(getValueSnapshot(form), null, 2)}</pre>
