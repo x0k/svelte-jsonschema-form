6b05388a30b06433b809f52ff7ddc90855fd822b
Roman Krasilnikov <x0kill@ya.ru>
2026-04-19 02:28:54 +0300

    [meta] Add collect extra fields script

---
+++ packages/meta/src/extra-fields.generated.ts
@@ -0,0 +1,72 @@
+export const extraFields = [
+  {
+    "file": "aggregated",
+    "name": "aggregatedField",
+    "wrapperOf": null
+  },
+  {
+    "file": "array-files",
+    "name": "arrayFilesField",
+    "wrapperOf": "files"
+  },
+  {
+    "file": "array-native-files",
+    "name": "arrayNativeFilesField",
+    "wrapperOf": "native-files"
+  },
+  {
+    "file": "array-tags",
+    "name": "arrayTagsField",
+    "wrapperOf": "tags"
+  },
+  {
+    "file": "boolean-select",
+    "name": "booleanSelectField",
+    "wrapperOf": null
+  },
+  {
+    "file": "enum",
+    "name": "enumField",
+    "wrapperOf": null
+  },
+  {
+    "file": "file",
+    "name": "fileField",
+    "wrapperOf": null
+  },
+  {
+    "file": "files",
+    "name": "filesField",
+    "wrapperOf": null
+  },
+  {
+    "file": "multi-enum",
+    "name": "multiEnumField",
+    "wrapperOf": null
+  },
+  {
+    "file": "native-file",
+    "name": "nativeFileField",
+    "wrapperOf": null
+  },
+  {
+    "file": "native-files",
+    "name": "nativeFilesField",
+    "wrapperOf": null
+  },
+  {
+    "file": "remote-enum",
+    "name": "remoteEnumField",
+    "wrapperOf": null
+  },
+  {
+    "file": "tags",
+    "name": "tagsField",
+    "wrapperOf": null
+  },
+  {
+    "file": "unknown-native-file",
+    "name": "unknownNativeFileField",
+    "wrapperOf": "native-file"
+  }
+] as const
