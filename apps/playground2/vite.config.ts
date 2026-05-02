c3a057eec5362612768fd7d37bf9575b240f12f1
Roman Krasilnikov <x0kill@ya.ru>
2026-05-02 15:49:28 +0300

    [playground] Add `ata` validator

--- apps/playground2/vite.config.ts
+++ apps/playground2/vite.config.ts
@@ -2,10 +2,16 @@ import { resolve } from "node:path";
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
+// import { visualizer } from "rollup-plugin-visualizer";
 
 export default defineConfig({
   base: "/svelte-jsonschema-form/playground3/",
-  plugins: [tailwindcss(), svelte()],
+  plugins: [
+    ,
+    // visualizer({})
+    tailwindcss(),
+    svelte(),
+  ],
   resolve: {
     alias: { "@": resolve(__dirname, "./src"), $lib: resolve("./src/lib") },
   },
