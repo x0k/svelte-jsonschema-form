c2bd28b5c36f1785e239e9e1f24850760ccb7702
Roman Krasilnikov <x0k@users.noreply.github.com>
2026-05-05 15:32:44 +0300

    [docs] Add `deprecated-keyword` example (#373)
    * [examples] Add `deprecated-keyword` example
    
    * [docs] Add `deprecated-keyword` example

---
+++ apps/docs2/src/web-ide/examples/deprecated-keyword.ts
@@ -0,0 +1,9 @@
+import sveltePage from "examples/basic-starter/src/routes/deprecated-keyword/+page.svelte?raw";
+
+import type { Layer } from "../layer";
+
+export const layer = {
+  files: {
+    "src/routes/+page.svelte": sveltePage,
+  },
+} satisfies Layer;
