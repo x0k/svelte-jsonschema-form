1c4e15fc27246b48934c8c4412642f113d6de647
Roman Krasilnikov <x0kill@ya.ru>
2026-05-25 22:45:50 +0300

    [examples] Merge compatible example folders, bump deps

--- examples/typebox-starter/src/routes/+layout.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-  import type { Snippet } from 'svelte';
-
-  import '@sjsf/basic-theme/css/basic.css';
-
-  const { children }: { children: Snippet } = $props()
-</script>
-
-{@render children()}
