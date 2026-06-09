3753f14337988b4bce519f579b895f74acd96745
Roman Krasilnikov <x0kill@ya.ru>
2026-06-09 14:33:11 +0300

    [lib] Fix `DeepPartial` type

--- packages/form/src/lib/types.ts
+++ packages/form/src/lib/types.ts
@@ -74,13 +74,11 @@ export type JsonPaths<T, D extends number = 10> = D extends -1
           >
         : never;
 
-export type DeepPartial<T> = T extends readonly [infer A, ...infer Rest]
-  ? readonly [DeepPartial<A>, ...DeepPartial<Rest>]
-  : T extends readonly (infer U)[]
-    ? readonly DeepPartial<U>[]
-    : T extends Record<string, unknown>
-      ? { [K in keyof T]?: DeepPartial<T[K]> }
-      : T;
+export type DeepPartial<T> = T extends readonly unknown[]
+  ? { [K in keyof T]: DeepPartial<T[K]> | undefined }
+  : T extends object
+    ? { [K in keyof T]?: DeepPartial<T[K]> }
+    : T;
 
 export type OmitItems<
   Args extends unknown[],
