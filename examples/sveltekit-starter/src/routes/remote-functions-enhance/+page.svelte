cf0798c6f8f7f873062680dbbb81ffd935e9e5bc
Roman Krasilnikov <x0kill@ya.ru>
2026-06-17 16:08:56 +0300

    [examples] Update remote function examples

--- examples/sveltekit-starter/src/routes/remote-functions-enhance/+page.svelte
+++ examples/sveltekit-starter/src/routes/remote-functions-enhance/+page.svelte
@@ -2,27 +2,25 @@
   import { BasicForm, createForm } from "@sjsf/form";
   import { connect } from "@sjsf/sveltekit/rf/client";
 
+  import type { CreatePost } from "$lib/post";
   import * as defaults from "$lib/sjsf/remote-defaults";
 
   import { createPost, getInitialData } from "./data.remote";
 
   const initialData = await getInitialData();
 
+  createPost.enhance(async ({ submit }) => {
+    if (await submit()) {
+      console.log(createPost.result);
+      form.reset();
+    }
+  });
+
   const form = createForm(
-    await connect(
-      createPost.enhance(async ({ submit }) => {
-        if (await submit()) {
-          console.log(createPost.result);
-          form.reset();
-        }
-      }),
-      {
-        ...defaults,
-        ...initialData,
-        // Required due to the use of `enhance`
-        fields: createPost.fields,
-      }
-    )
+    await connect<CreatePost>(createPost, {
+      ...defaults,
+      ...initialData,
+    })
   );
 </script>
 
