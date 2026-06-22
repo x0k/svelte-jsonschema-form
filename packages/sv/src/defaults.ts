428ff3943fd0a27d2787cd18ebe60b4047a0c03b
Roman Krasilnikov <x0kill@ya.ru>
2026-06-23 02:42:23 +0300

    [sv] Update `meta` package usage

--- packages/sv/src/defaults.ts
+++ packages/sv/src/defaults.ts
@@ -1,10 +1,6 @@
-import {
-  createDefaults,
-  KIT_PATH_FACTORY,
-  type PathFactory,
-} from "meta/codegen";
+import { createDefaults } from "meta/codegen";
 
-import { POST_EXTRA_WIDGETS, POST_MODEL_NAME, type Context } from "./model.js";
+import { POST_EXTRA_WIDGETS, type Context } from "./model.js";
 
 export function defaultsTs({
   options,
@@ -14,17 +10,8 @@ export function defaultsTs({
   directory,
   ts,
   js,
-  isKit,
-  file,
 }: Context) {
   const filepath = `${directory.lib}/sjsf/defaults.${language}`;
-  const lib: PathFactory = isKit
-    ? KIT_PATH_FACTORY
-    : (path) =>
-        file.getRelative({
-          from: filepath,
-          to: `${directory.lib}/${path}`,
-        });
 
   sv.file(
     filepath,
@@ -33,9 +20,7 @@ export function defaultsTs({
       isTs,
       ts,
       js,
-      lib,
       resolver: "inline",
-      modelName: POST_MODEL_NAME,
       widgets: POST_EXTRA_WIDGETS,
       focusOnFirstError: true,
       merger: {},
