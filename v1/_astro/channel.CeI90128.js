d8313f57cebae4f9592744a86f43f39943500b47
x0k <x0k@users.noreply.github.com>
2026-07-27 09:09:40 +0000

    deploy: 879f69a50b20757718bf90db189fbc9ff0037749

--- v1/_astro/channel.CeI90128.js
+++
@@ -1,1 +0,0 @@
-import{U as a,C as n}from"./_fields-hierarchy.astro_astro_type_script_index_0_lang.DFQVf8HU.js";const t=(r,o)=>a.lang.round(n.parse(r)[o]);export{t as c};
--- v2/_astro/single.nK3uullM.js
+++
@@ -1,1 +0,0 @@
-const i={schema:{title:"A single-field form",type:"string"},initialValue:"initial value",uiSchema:{}};export{i as default};
