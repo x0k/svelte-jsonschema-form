8f24cf0e728ef1ea433649dbfe96857cca365008
Roman Krasilnikov <x0kill@ya.ru>
2026-09-02 10:49:47 +0300

    Update validator-testing and zod4-validator for simplified validator interface

--- packages/zod4-validator/src/model.ts
+++ packages/zod4-validator/src/model.ts
@@ -9,6 +9,11 @@ export type AugmentedSchemaFactory = (
   schema: $ZodType
 ) => $ZodTypes | undefined;
 
+export type ConditionSchemaFactory = (
+  key: string,
+  propSchema: $ZodTypes
+) => $ZodTypes;
+
 export interface SchemaProvider {
   schema: Schema;
 }
